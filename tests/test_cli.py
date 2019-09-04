ac62fe24cf4e1cd8d3bfebd2f7c8254c709c53c3
MarcoFavorito <marco.favorito@gmail.com>
2019-09-04 16:59:42 +0100

    add simple test for 'aea' cli.

---
+++ tests/test_cli.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This test module contains the tests for the `aea` command-line tool."""
+
+
+from click.testing import CliRunner
+from aea.cli import cli
+
+
+def test_no_argument():
+    """Test that if we run the cli tool without arguments, it exits gracefully."""
+    runner = CliRunner()
+    result = runner.invoke(cli, [])
+    assert result.exit_code == 0
+
+
