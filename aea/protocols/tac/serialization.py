d1839e0e21b310f40f3433c556622cb6b419cb35
MarcoFavorito <marco.favorito@gmail.com>
2019-09-11 19:17:58 +0200

    apply analogous changes to TAC serializer. Add 'scripts' folder for mypy checks.

--- aea/protocols/tac/serialization.py
+++ aea/protocols/tac/serialization.py
@@ -224,5 +224,5 @@ class TACSerializer(Serializer):
             raise ValueError("Type not recognized.")
 
         new_body["type"] = TACMessage.Type(new_body["type"])
-        tac_message = TACMessage(body=new_body)
+        tac_message = TACMessage(tac_type=tac_type, body=new_body)
         return tac_message
