fea16e7eccc1ad5ff2f25c9d66a0acaa7bc791bd
MarcoFavorito <marco.favorito@gmail.com>
2019-09-30 23:29:04 +0200

    add implementation of constraint ts.

--- tests/test_aea.py
+++ tests/test_aea.py
@@ -32,14 +32,15 @@ import time
 from threading import Thread
 from pathlib import Path
 
+from .conftest import CUR_PATH
+
 
 def test_initialiseAeA():
     """Tests the initialisation of the AeA."""
     node = LocalNode()
     public_key_1 = "mailbox1"
-    path = "/tests/aea/"
     mailbox1 = MailBox(OEFLocalConnection(public_key_1, node))
-    myAea = AEA("Agent0", mailbox1, directory=str(Path(".").absolute()) + path)
+    myAea = AEA("Agent0", mailbox1, directory=str(Path(CUR_PATH, "aea")))
     assert AEA("Agent0", mailbox1), "Agent is not inisialised"
     print(myAea.context)
     assert myAea.context == myAea._context, "Cannot access the Agent's Context"
@@ -52,7 +53,6 @@ def test_act():
     """Tests the act function of the AeA."""
     node = LocalNode()
     agent_name = "MyAgent"
-    path = "/tests/data/dummy_aea/"
     private_key_pem_path = _create_temporary_private_key_pem_path()
     crypto = Crypto(private_key_pem_path=private_key_pem_path)
     public_key = crypto.public_key
@@ -62,7 +62,7 @@ def test_act():
         agent_name,
         mailbox,
         private_key_pem_path=private_key_pem_path,
-        directory=str(Path(".").absolute()) + path)
+        directory=str(Path(CUR_PATH, "data", "dummy_aea")))
     t = Thread(target=agent.start)
     t.start()
     time.sleep(1)
@@ -77,7 +77,6 @@ def test_react():
     """Tests income messages."""
     node = LocalNode()
     agent_name = "MyAgent"
-    path = "/tests/data/dummy_aea/"
     private_key_pem_path = _create_temporary_private_key_pem_path()
     crypto = Crypto(private_key_pem_path=private_key_pem_path)
     public_key = crypto.public_key
@@ -96,7 +95,7 @@ def test_react():
         agent_name,
         mailbox,
         private_key_pem_path=private_key_pem_path,
-        directory=str(Path(".").absolute()) + path)
+        directory=str(Path(CUR_PATH, "data", "dummy_aea")))
     t = Thread(target=agent.start)
     t.start()
     agent.mailbox.inbox._queue.put(envelope)
@@ -113,7 +112,6 @@ def test_handle():
     """Tests handle method of an agent."""
     node = LocalNode()
     agent_name = "MyAgent"
-    path = "/tests/data/dummy_aea/"
     private_key_pem_path = _create_temporary_private_key_pem_path()
     crypto = Crypto(private_key_pem_path=private_key_pem_path)
     public_key = crypto.public_key
@@ -132,11 +130,11 @@ def test_handle():
         agent_name,
         mailbox,
         private_key_pem_path=private_key_pem_path,
-        directory=str(Path(".").absolute()) + path)
+        directory=str(Path(CUR_PATH, "data", "dummy_aea")))
     t = Thread(target=agent.start)
     t.start()
     agent.mailbox.inbox._queue.put(envelope)
-    env = agent.mailbox.outbox._queue.get(block=True, timeout=1)
+    env = agent.mailbox.outbox._queue.get(block=True, timeout=5.0)
     assert env.protocol_id == "default",\
         "The envelope is not the expected protocol (Unsupported protocol)"
 
