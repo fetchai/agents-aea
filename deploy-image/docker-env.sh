bf6e44457815e6e2cc9de3b800772fa119f601e8
David Minarsch <david.minarsch@googlemail.com>
2019-10-19 09:03:48 +0200

    Prepare develop for release 0.1.10

--- deploy-image/docker-env.sh
+++ deploy-image/docker-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Swap the following lines if you want to work with 'latest'
-DOCKER_IMAGE_TAG=aea-deploy:0.1.9
+DOCKER_IMAGE_TAG=aea-deploy:0.1.10
 # DOCKER_IMAGE_TAG=aea-deploy:latest
 
 DOCKER_BUILD_CONTEXT_DIR=..
