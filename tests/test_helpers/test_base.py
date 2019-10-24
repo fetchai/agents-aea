36921ff73caae52990b7175cef85bcb44c6316fe
MarcoFavorito <marco.favorito@gmail.com>
2019-10-24 12:53:28 +0200

    100% on 'aea/helpers/base.py'

--- tests/test_helpers/test_base.py
+++ tests/test_helpers/test_base.py
@@ -21,6 +21,7 @@
 
 import sys
 
+from aea.connections.oef.connection import OEFConnection
 from aea.helpers.base import locate
 
 
@@ -28,8 +29,32 @@ class TestHelpersBase:
     """Test the helper functions."""
 
     def test_locate(self):
-        """Test the locate function."""
+        """Test the locate function to locate modules."""
         sys.modules["gym_connection"] = locate("packages.connections.gym")
         assert sys.modules['gym_connection'] is not None
         sys.modules["gym_connection"] = locate("packages.connections.weather")
         assert sys.modules['gym_connection'] is None
+
+    def test_locate_class(self):
+        """Test the locate function to locate classes."""
+
+        expected_class = OEFConnection
+        actual_class = locate("aea.connections.oef.connection.OEFConnection")
+        # although they are the same class, they are different instances in memory
+        # and the build-in default "__eq__" method does not compare the attributes.
+        # so compare the names
+        assert actual_class is not None
+        assert expected_class.__name__ == actual_class.__name__
+
+    def test_locate_with_builtins(self):
+        """Test that locate function returns the built-in."""
+        result = locate("int.bit_length")
+        assert int.bit_length == result
+
+    def test_locate_when_path_does_not_exist(self):
+        """Test that locate function returns None when the dotted path does not exist."""
+        result = locate("aea.not.existing.path")
+        assert result is None
+
+        result = locate("ThisClassDoesNotExist")
+        assert result is None
