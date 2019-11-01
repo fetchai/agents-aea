c486b506acda37de385071bca34bd89d74d2d5b3
Aristotelis <aristotelis.30fyllidis@gmail.com>
2019-11-01 09:28:51 +0000

    crypt_tests WIP

--- tests/test_crypto/test_helpers.py
+++ tests/test_crypto/test_helpers.py
@@ -19,12 +19,17 @@
 # ------------------------------------------------------------------------------
 
 """This module contains the tests for the crypto/helpers module."""
+import logging
+
 import os
 import pytest
-
 from aea.crypto.helpers import _try_validate_private_key_pem_path, _try_validate_fet_private_key_path, \
     _try_validate_ethereum_private_key_path
+
 from tests.conftest import CUR_PATH
+
+
+logger = logging.getLogger(__name__)
 
 
 class TestHelperFile:
