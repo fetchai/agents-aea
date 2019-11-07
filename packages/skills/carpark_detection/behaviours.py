88aaedb4f1486659096a6d0c5f1da9a154b11613
Diarmid Campbell <dishmopcampbell@hotmail.com>
2019-11-07 16:40:25 +0000

    fixed status issues in car park skill

--- packages/skills/carpark_detection/behaviours.py
+++ packages/skills/carpark_detection/behaviours.py
@@ -125,7 +125,8 @@ class ServiceRegistrationBehaviour(Behaviour):
         """Initialise the behaviour."""
         self._services_interval = kwargs.pop('services_interval', 30)  # type: int
         super().__init__(**kwargs)
-        self._last_connection_status = self.connection_status.is_connected
+        self._last_connection_status = self.context.connection_status.is_connected
+        print("__init__ self._last_connection_status {}".format(self._last_connection_status))
         self._last_update_time = datetime.datetime.now()  # type: datetime.datetime
         self._registered_service_description = None  # type: Optional[Description]
         self._oef_msf_id = 0
@@ -137,7 +138,7 @@ class ServiceRegistrationBehaviour(Behaviour):
         :return: None
         """
         strategy = cast(Strategy, self.context.strategy)
-        strategy.db.set_system_status("oef-status", "Disconnected")
+        self._record_oef_status()
         balance = self.context.ledger_apis.token_balance('fetchai', cast(str, self.context.agent_addresses.get('fetchai')))
         strategy.db.set_system_status("ledger-status", self.context.ledger_apis.last_tx_statuses['fetchai'])
         logger.info("[{}]: starting balance on fetchai ledger={}.".format(self.context.agent_name, balance))
@@ -215,12 +216,16 @@ class ServiceRegistrationBehaviour(Behaviour):
         """
         if self.context.connection_status.is_connected != self._last_connection_status:
             self._last_connection_status = self.context.connection_status.is_connected
-            strategy = cast(Strategy, self.context.strategy)
-            if self._last_connection_status:
-                strategy.db.set_system_status("oef-status", "Connected")
-            else:
-                strategy.db.set_system_status("oef-status", "Disconnected")
+            self._record_oef_status()
 
+    def _record_oef_status(self):
+        print("********_record_oef_status self._last_connection_status {}".format(self._last_connection_status))
+        strategy = cast(Strategy, self.context.strategy)
+        if self._last_connection_status:
+            strategy.db.set_system_status("oef-status", "Connected")
+        else:
+            strategy.db.set_system_status("oef-status", "Disconnected")
+            
     def teardown(self) -> None:
         """
         Implement the task teardown.
