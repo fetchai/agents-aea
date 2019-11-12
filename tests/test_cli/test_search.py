1d6921b19eda189120cb563f2c321f25e7ca1349
Diarmid Campbell <dishmopcampbell@hotmail.com>
2019-11-12 15:00:45 +0000

    tests for search updated

--- tests/test_cli/test_search.py
+++ tests/test_cli/test_search.py
@@ -46,28 +46,46 @@ class TestSearchProtocols:
         cls.runner = CliRunner()
 
     def _generated_expected_output(self):
-        expected_output = "Available protocols:\n"
-        expected_output += "default\t[The default protocol allows for any bytes message.]\n"
-        expected_output += "fipa\t[The fipa protocol implements the FIPA ACL.]\n"
-        expected_output += "gym\t[The gym protocol implements the messages an agent needs to engage with a gym connection.]\n"
-        expected_output += "oef\t[The oef protocol implements the OEF specific messages.]\n"
-        expected_output += "scaffold\t[The scaffold protocol scaffolds a protocol to be implemented by the developer.]\n"
-        expected_output += "tac\t[The tac protocol implements the messages an AEA needs to participate in the TAC.]\n"
-        return expected_output
+        return """Available protocols:
+------------------------------
+Name: default
+Description: The default protocol allows for any bytes message.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: fipa
+Description: The fipa protocol implements the FIPA ACL.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: gym
+Description: The gym protocol implements the messages an agent needs to engage with a gym connection.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: oef
+Description: The oef protocol implements the OEF specific messages.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: tac
+Description: The tac protocol implements the messages an AEA needs to participate in the TAC.
+Version: 0.1.0
+------------------------------
+
+"""
 
     def test_correct_output_default_registry(self):
         """Test that the command has printed the correct output when using the default registry."""
         os.chdir(AEA_DIR)
         self.result = self.runner.invoke(cli, [*CLI_LOG_OPTION, "search", "protocols"])
-
         assert self.result.output == self._generated_expected_output()
 
     def test_correct_output_custom_registry(self):
         """Test that the command has printed the correct output when using a custom registry."""
         os.chdir(AEA_DIR)
         self.result = self.runner.invoke(cli, [*CLI_LOG_OPTION, "search", "--registry", DEFAULT_REGISTRY_PATH, "protocols"])
-        test = self._generated_expected_output()
-        assert self.result.output == test
+        assert self.result.output == self._generated_expected_output()
 
     @classmethod
     def teardown_class(cls):
@@ -88,17 +106,40 @@ class TestSearchConnections:
         cls.cwd = os.getcwd()
         cls.runner = CliRunner()
 
-
     def _generated_expected_output(self):
-        expected_output = "Available connections:\n"
-        expected_output += "gym\t[The gym connection wraps an OpenAI gym.]\n"
-        expected_output += "local\t[The local connection provides a stub for an OEF node.]\n"
-        expected_output += "oef\t[The oef connection provides a wrapper around the OEF sdk.]\n"
-        expected_output += "p2p\t[The p2p connection provides a connection with the fetch.ai mail provider.]\n"
-        expected_output += "scaffold\t[The scaffold connection provides a scaffold for a connection to be implemented by the developer.]\n"
-        expected_output += "stub\t[The stub connection implements a connection stub which reads/writes messages from/to file.]\n"
-        expected_output += "tcp\t[None]\n"
-        return expected_output
+        return """Available connections:
+------------------------------
+Name: gym
+Description: The gym connection wraps an OpenAI gym.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: local
+Description: The local connection provides a stub for an OEF node.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: oef
+Description: The oef connection provides a wrapper around the OEF sdk.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: p2p
+Description: The p2p connection provides a connection with the fetch.ai mail provider.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: stub
+Description: The stub connection implements a connection stub which reads/writes messages from/to file.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: tcp
+Description: None
+Version: 0.1.0
+------------------------------
+
+"""
 
     def test_correct_output_default_registry(self):
         """Test that the command has printed the correct output when using the default registry."""
@@ -132,35 +173,75 @@ class TestSearchSkills:
         cls.runner = CliRunner()
 
     def _generated_expected_output(self):
-        expected_output = "Available skills:\n"
-        expected_output += "carpark_client\t[None]\n"
-        expected_output += "carpark_detection\t[None]\n"
-        expected_output += "echo\t[The echo skill implements simple echo functionality.]\n"
-        expected_output += "error\t[The error skill implements basic error handling required by all AEAs.]\n"
-        expected_output += "fipa_negotiation\t[The fipa skill implements the logic for an AEA to do fipa negotiation.]\n"
-        expected_output += "gym\t[The gym skill wraps an RL agent.]\n"
-        expected_output += "scaffold\t[The scaffold skill is a scaffold for your own skill implementation.]\n"
-        expected_output += "tac\t[The tac skill implements the logic for an AEA to participate in the TAC.]\n"
-        expected_output += "weather_client\t[None]\n"
-        expected_output += "weather_client_ledger\t[None]\n"
-        expected_output += "weather_station\t[None]\n"
-        expected_output += "weather_station_ledger\t[None]\n"
+        return """Available skills:
+------------------------------
+Name: carpark_client
+Description: None
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: carpark_detection
+Description: None
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: echo
+Description: The echo skill implements simple echo functionality.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: error
+Description: The error skill implements basic error handling required by all AEAs.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: fipa_negotiation
+Description: The fipa skill implements the logic for an AEA to do fipa negotiation.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: gym
+Description: The gym skill wraps an RL agent.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: tac
+Description: The tac skill implements the logic for an AEA to participate in the TAC.
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: weather_client
+Description: None
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: weather_client_ledger
+Description: None
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: weather_station
+Description: None
+Version: 0.1.0
+------------------------------
+------------------------------
+Name: weather_station_ledger
+Description: None
+Version: 0.1.0
+------------------------------
 
-        return expected_output
+"""
 
     def test_correct_output_default_registry(self):
         """Test that the command has printed the correct output when using the default registry."""
         os.chdir(AEA_DIR)
         self.result = self.runner.invoke(cli, [*CLI_LOG_OPTION, "search", "skills"])
-
-        test = self._generated_expected_output()
         assert self.result.output == self._generated_expected_output()
 
     def test_correct_output_custom_registry(self):
         """Test that the command has printed the correct output when using a custom registry."""
         os.chdir(AEA_DIR)
         self.result = self.runner.invoke(cli, [*CLI_LOG_OPTION, "search", "--registry", DEFAULT_REGISTRY_PATH, "skills"])
-
         assert self.result.output == self._generated_expected_output()
 
     @classmethod
