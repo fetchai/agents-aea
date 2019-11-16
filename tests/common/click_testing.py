c8eba190bf6bb210d292872a58c32179f0acc559
MarcoFavorito <marco.favorito@gmail.com>
2019-11-16 15:34:08 +0100

    add license and module description in 'tests.common.click_testing.py'

--- tests/common/click_testing.py
+++ tests/common/click_testing.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""
+This common module is an adaptation of click.testing.
+
+It is an almost exact copy of click.testing, but it fixes an issue in
+CLIRunner.invoke, in the 'finally' clause. More precisely, before reading from
+the testing outstream, it checks whether it has been already closed.
+"""
+
 import os
 import sys
 import shutil
