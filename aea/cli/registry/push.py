107b316d2efeae4ede337c055bbcc605ab572999
Oleg Panasevych <panasevychol@gmail.com>
2019-12-12 14:12:25 +0200

    Echo messages are back  as logger.debug.

--- aea/cli/registry/push.py
+++ aea/cli/registry/push.py
@@ -22,6 +22,7 @@ import os
 import tarfile
 import shutil
 
+from aea.cli.common import logger
 from aea.cli.registry.utils import request_api, load_yaml, clean_tarfiles
 
 
@@ -52,6 +53,10 @@ def push_item(item_type: str, item_name: str) -> None:
 
     items_folder = os.path.join(cwd, item_type_plural)
     item_path = os.path.join(items_folder, item_name)
+    logger.debug(
+        'Searching for {} {} in {} ...'
+        .format(item_name, item_type, items_folder)
+    )
     if not os.path.exists(item_path):
         raise click.ClickException(
             '{} "{}" not found  in {}. Make sure you run push command '
@@ -61,10 +66,15 @@ def push_item(item_type: str, item_name: str) -> None:
         )
 
     output_filename = '{}.tar.gz'.format(item_name)
+    logger.debug(
+        'Compressing {} {} to {} ...'
+        .format(item_name, item_type, output_filename)
+    )
     _compress(output_filename, item_path)
     output_filepath = os.path.join(cwd, output_filename)
 
     item_config_filepath = os.path.join(item_path, '{}.yaml'.format(item_type))
+    logger.debug('Reading {} {} config ...'.format(item_name, item_type))
     item_config = load_yaml(item_config_filepath)
 
     data = {
@@ -73,6 +83,7 @@ def push_item(item_type: str, item_name: str) -> None:
         'version': item_config['version']
     }
     path = '/{}/create'.format(item_type_plural)
+    logger.debug('Pushing {} {} to Registry ...'.format(item_name, item_type))
     resp = request_api(
         'POST', path, data=data, auth=True, filepath=output_filepath
     )
