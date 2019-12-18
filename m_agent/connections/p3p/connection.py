4c44994cd64c8ed5324062e3ed7492e388c9ac35
Aristotelis <aristotelis.30fyllidis@gmail.com>
2019-12-18 09:01:55 +0000

    generate uuid on scaffold

---
+++ m_agent/connections/p3p/connection.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Scaffold connection and channel."""
+
+import logging
+from typing import Optional, Set
+
+from aea.configurations.base import ConnectionConfig
+from aea.connections.base import Connection
+from aea.mail.base import Envelope, Address
+
+logger = logging.getLogger(__name__)
+
+
+class MyScaffoldConnection(Connection):
+    """Proxy to the functionality of the SDK or API."""
+
+    restricted_to_protocols = set()  # type: Set[str]
+    excluded_protocols = set()  # type: Set[str]
+
+    def __init__(self, connection_id: str, address: Address, *args, **kwargs):
+        """
+        Initialize a connection to an SDK or API.
+
+        :param connection_id: the identifier of the connection object.
+        :param address: the address used in the protocols.
+        """
+        super().__init__(connection_id, *args, **kwargs)
+        self.address = address
+
+    async def connect(self) -> None:
+        """
+        Set up the connection.
+
+        In the implementation, remember to update 'connection_status' accordingly.
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    async def disconnect(self) -> None:
+        """
+        Tear down the connection.
+
+        In the implementation, remember to update 'connection_status' accordingly.
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    async def send(self, envelope: 'Envelope') -> None:
+        """
+        Send an envelope.
+
+        :param envelope: the envelope to send.
+        :return: None
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    async def receive(self, *args, **kwargs) -> Optional['Envelope']:
+        """
+        Receive an envelope. Blocking.
+
+        :return: the envelope received, or None.
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    @classmethod
+    def from_config(cls, address: Address, connection_configuration: ConnectionConfig) -> 'Connection':
+        """
+        Get the Gym connection from the connection configuration.
+
+        :param address: the address of the agent.
+        :param connection_configuration: the connection configuration object.
+        :return: the connection object
+        """
+        raise NotImplementedError  # pragma: no cover
