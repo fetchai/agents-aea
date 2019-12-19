5ffe68d6649b1909a0410c9dad02b38e4368e79b
Aristotelis <aristotelis.30fyllidis@gmail.com>
2019-12-19 08:10:17 +0000

    reverted back to author and changed public_id to fingerprint

--- aea/helpers/base.py
+++ aea/helpers/base.py
@@ -72,10 +72,10 @@ def locate(path):
     return object
 
 
-def generate_public_id(user_name: str, package_name: str, version: str, nonce: Optional[int] = None) -> str:
+def generate_fingerprint(author: str, package_name: str, version: str, nonce: Optional[int] = None) -> str:
     """Generate a unique id for the package."""
     import hashlib
-    string_for_hash = "".join([user_name, package_name, version, str(nonce)])
+    string_for_hash = "".join([author, package_name, version, str(nonce)])
     m_hash = hashlib.sha3_256()
     m_hash.update(string_for_hash.encode())
     encoded_str = m_hash.digest().hex()
