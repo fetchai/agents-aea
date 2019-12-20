992ad198b36fd0765bbb1dae9a707c8adbd2adbb
Aristotelis <aristotelis.30fyllidis@gmail.com>
2019-12-20 15:29:55 +0000

    Reverted the changes

--- aea/configurations/base.py
+++ aea/configurations/base.py
@@ -46,7 +46,6 @@ Dependency = dict
 A dictionary from package name to dependency data structure (see above).
 The package name must satisfy the constraints on Python packages names.
 For details, see https://www.python.org/dev/peps/pep-0426/#name.
-
 The main advantage of having a dictionary is that we implicitly filter out dependency duplicates.
 We cannot have two items with the same package name since the keys of a YAML object form a set.
 """
@@ -80,7 +79,6 @@ class CRUDCollection(Generic[T]):
     def create(self, item_id: str, item: T) -> None:
         """
         Add an item.
-
         :param item_id: the item id.
         :param item: the item to be added.
         :return: None
@@ -94,7 +92,6 @@ class CRUDCollection(Generic[T]):
     def read(self, item_id: str) -> Optional[T]:
         """
         Get an item by its name.
-
         :param item_id: the item id.
         :return: the associated item, or None if the item id is not present.
         """
@@ -103,7 +100,6 @@ class CRUDCollection(Generic[T]):
     def update(self, item_id: str, item: T) -> None:
         """
         Update an existing item.
-
         :param item_id: the item id.
         :param item: the item to be added.
         :return: None
@@ -150,26 +146,31 @@ class PrivateKeyPathConfig(Configuration):
 class LedgerAPIConfig(Configuration):
     """Handle a ledger api configuration."""
 
-    def __init__(self, ledger_name: str = "", **args):
-        """Initialize a ledger class configuration."""
-        self.ledger_name = ledger_name
-        self.args = args
+    def __init__(self, ledger: str = "", addr: str = "", port: int = 1000):
+        """Initialize a handler configuration."""
+        self.ledger = ledger
+        self.addr = addr
+        self.port = port
 
     @property
     def json(self) -> Dict:
         """Return the JSON representation."""
         return {
-            "ledger_name": self.ledger_name,
-            "args": self.args
+            "ledger": self.ledger,
+            "addr": self.addr,
+            "port": self.port
         }
 
     @classmethod
     def from_json(cls, obj: Dict):
         """Initialize from a JSON object."""
-        class_name = cast(str, obj.get("ledger_name"))
-        return SharedClassConfig(
-            class_name=class_name,
-            **obj.get("args", {})
+        ledger = cast(str, obj.get("ledger"))
+        addr = cast(str, obj.get("addr"))
+        port = cast(int, obj.get("port"))
+        return LedgerAPIConfig(
+            ledger=ledger,
+            addr=addr,
+            port=port
         )
 
 
@@ -503,6 +504,7 @@ class AgentConfig(Configuration):
                  registry_path: str = "",
                  description: str = "",
                  private_key_paths: Dict[str, str] = None,
+                 ledger_apis: Dict[str, Tuple[str, int]] = None,
                  logging_config: Optional[Dict] = None):
         """Instantiate the agent configuration object."""
         self.agent_name = agent_name
@@ -520,6 +522,10 @@ class AgentConfig(Configuration):
         private_key_paths = private_key_paths if private_key_paths is not None else {}
         for ledger, path in private_key_paths.items():
             self.private_key_paths.create(ledger, PrivateKeyPathConfig(ledger, path))
+
+        ledger_apis = ledger_apis if ledger_apis is not None else {}
+        for ledger, (addr, port) in ledger_apis.items():
+            self.ledger_apis.create(ledger, LedgerAPIConfig(ledger, addr, port))
 
         self.logging_config = logging_config if logging_config is not None else {}
         self._default_connection = None  # type: Optional[str]
@@ -541,7 +547,6 @@ class AgentConfig(Configuration):
     def default_connection(self, connection_name: str):
         """
         Set the default connection.
-
         :param connection_name: the name of the default connection.
         :return: None
         """
@@ -561,7 +566,7 @@ class AgentConfig(Configuration):
             "registry_path": self.registry_path,
             "description": self.description,
             "private_key_paths": [{"private_key_path": p.json} for l, p in self.private_key_paths.read_all()],
-            "ledger_apis": {key: l.json for key, l in self.ledger_apis.read_all()},
+            "ledger_apis": [{"ledger_api": t.json} for l, t in self.ledger_apis.read_all()],
             "logging_config": self.logging_config,
             "default_connection": self.default_connection,
             "connections": sorted(self.connections),
@@ -577,6 +582,11 @@ class AgentConfig(Configuration):
             private_key_path = PrivateKeyPathConfig.from_json(p["private_key_path"])
             private_key_paths[private_key_path.ledger] = private_key_path.path
 
+        ledger_apis = {}
+        for l in obj.get("ledger_apis", []):  # type: ignore
+            ledger_api = LedgerAPIConfig.from_json(l["ledger_api"])
+            ledger_apis[ledger_api.ledger] = (ledger_api.addr, ledger_api.port)
+
         agent_config = AgentConfig(
             agent_name=cast(str, obj.get("agent_name")),
             aea_version=cast(str, obj.get("aea_version")),
@@ -588,11 +598,8 @@ class AgentConfig(Configuration):
             description=cast(str, obj.get("description", "")),
             logging_config=cast(Dict, obj.get("logging_config", {})),
             private_key_paths=cast(Dict, private_key_paths),
+            ledger_apis=cast(Dict, ledger_apis)
         )
-
-        for ledger_id, ledger_data in obj.get("ledger_apis", {}).items():  # type: ignore
-            ledger_config = LedgerAPIConfig.from_json(ledger_data)
-            agent_config.ledger_apis.create(ledger_id, ledger_config)
 
         agent_config.connections = set(cast(List[str], obj.get("connections")))
         agent_config.protocols = set(cast(List[str], obj.get("protocols")))
