939d5a701350fb65c1ce2c71a80bb3c9a2e04384
Aristotelis <aristotelis.30fyllidis@gmail.com>
2020-02-03 13:18:22 +0000

    First draft: creates generic_agent && generic_client

---
+++ packages/fetchai/skills/generic_buyer/strategy.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the strategy class."""
+
+from typing import cast
+
+from aea.helpers.search.models import Constraint, ConstraintType, Description, Query
+from aea.skills.base import SharedClass
+
+DEFAULT_COUNTRY = "UK"
+SEARCH_TERM = "country"
+DEFAULT_MAX_PRICE = 5
+DEFAULT_MAX_BUYER_TX_FEE = 2
+DEFAULT_CURRENCY_PBK = "FET"
+DEFAULT_LEDGER_ID = "fetchai"
+DEFAULT_IS_LEDGER_TX = True
+
+
+class Strategy(SharedClass):
+    """This class defines a strategy for the agent."""
+
+    def __init__(self, **kwargs) -> None:
+        """
+        Initialize the strategy of the agent.
+
+        :return: None
+        """
+        self._country = kwargs.pop("country", DEFAULT_COUNTRY)
+        self._max_price = kwargs.pop("max_price", DEFAULT_MAX_PRICE)
+        self.max_buyer_tx_fee = kwargs.pop("max_buyer_tx_fee", DEFAULT_MAX_BUYER_TX_FEE)
+        self._currency_id = kwargs.pop("currency_id", DEFAULT_CURRENCY_PBK)
+        self._ledger_id = kwargs.pop("ledger_id", DEFAULT_LEDGER_ID)
+        self.is_ledger_tx = kwargs.pop("is_ledger_tx", DEFAULT_IS_LEDGER_TX)
+        super().__init__(**kwargs)
+        self._search_id = 0
+        self.is_searching = True
+
+    def get_next_search_id(self) -> int:
+        """
+        Get the next search id and set the search time.
+
+        :return: the next search id
+        """
+        self._search_id += 1
+        return self._search_id
+
+    def get_service_query(self) -> Query:
+        """
+        Get the service query of the agent.
+
+        :return: the query
+        """
+        query = Query(
+            [Constraint(SEARCH_TERM, ConstraintType("==", self._country))], model=None
+        )
+        return query
+
+    def is_acceptable_proposal(self, proposal: Description) -> bool:
+        """
+        Check whether it is an acceptable proposal.
+
+        :return: whether it is acceptable
+        """
+        result = (
+            (proposal.values["price"] - proposal.values["seller_tx_fee"] > 0)
+            and (proposal.values["price"] <= self._max_price)
+            and (proposal.values["currency_id"] == self._currency_id)
+            and (proposal.values["ledger_id"] == self._ledger_id)
+        )
+        return result
+
+    def is_affordable_proposal(self, proposal: Description) -> bool:
+        """
+        Check whether it is an affordable proposal.
+
+        :return: whether it is affordable
+        """
+        if self.is_ledger_tx:
+            payable = proposal.values["price"] + self.max_buyer_tx_fee
+            ledger_id = proposal.values["ledger_id"]
+            address = cast(str, self.context.agent_addresses.get(ledger_id))
+            balance = self.context.ledger_apis.token_balance(ledger_id, address)
+            result = balance >= payable
+        else:
+            result = True
+        return result
---
+++ packages/fetchai/skills/generic_seller/behaviours.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This package contains a scaffold of a behaviour."""
+
+import logging
+from typing import Optional, cast
+
+from aea.crypto.ethereum import ETHEREUM
+from aea.crypto.fetchai import FETCHAI
+from aea.helpers.search.models import Description
+from aea.skills.behaviours import TickerBehaviour
+
+from packages.fetchai.protocols.oef.message import OEFMessage
+from packages.fetchai.protocols.oef.serialization import DEFAULT_OEF, OEFSerializer
+from packages.fetchai.skills.generic_seller.strategy import Strategy
+
+logger = logging.getLogger("aea.weather_station_skill")
+
+SERVICE_ID = ""
+DEFAULT_SERVICES_INTERVAL = 30.0
+
+
+class ServiceRegistrationBehaviour(TickerBehaviour):
+    """This class implements a behaviour."""
+
+    def __init__(self, **kwargs):
+        """Initialise the behaviour."""
+        services_interval = kwargs.pop(
+            "services_interval", DEFAULT_SERVICES_INTERVAL
+        )  # type: int
+        super().__init__(tick_interval=services_interval, **kwargs)
+        self._registered_service_description = None  # type: Optional[Description]
+
+    def setup(self) -> None:
+        """
+        Implement the setup.
+
+        :return: None
+        """
+        if self.context.ledger_apis.has_fetchai:
+            fet_balance = self.context.ledger_apis.token_balance(
+                FETCHAI, cast(str, self.context.agent_addresses.get(FETCHAI))
+            )
+            if fet_balance > 0:
+                logger.info(
+                    "[{}]: starting balance on fetchai ledger={}.".format(
+                        self.context.agent_name, fet_balance
+                    )
+                )
+            else:
+                logger.warning(
+                    "[{}]: you have no starting balance on fetchai ledger!".format(
+                        self.context.agent_name
+                    )
+                )
+
+        if self.context.ledger_apis.has_ethereum:
+            eth_balance = self.context.ledger_apis.token_balance(
+                ETHEREUM, cast(str, self.context.agent_addresses.get(ETHEREUM))
+            )
+            if eth_balance > 0:
+                logger.info(
+                    "[{}]: starting balance on ethereum ledger={}.".format(
+                        self.context.agent_name, eth_balance
+                    )
+                )
+            else:
+                logger.warning(
+                    "[{}]: you have no starting balance on ethereum ledger!".format(
+                        self.context.agent_name
+                    )
+                )
+
+        self._register_service()
+
+    def act(self) -> None:
+        """
+        Implement the act.
+
+        :return: None
+        """
+        self._unregister_service()
+        self._register_service()
+
+    def teardown(self) -> None:
+        """
+        Implement the task teardown.
+
+        :return: None
+        """
+        if self.context.ledger_apis.has_fetchai:
+            balance = self.context.ledger_apis.token_balance(
+                FETCHAI, cast(str, self.context.agent_addresses.get(FETCHAI))
+            )
+            logger.info(
+                "[{}]: ending balance on fetchai ledger={}.".format(
+                    self.context.agent_name, balance
+                )
+            )
+
+        if self.context.ledger_apis.has_ethereum:
+            balance = self.context.ledger_apis.token_balance(
+                ETHEREUM, cast(str, self.context.agent_addresses.get(ETHEREUM))
+            )
+            logger.info(
+                "[{}]: ending balance on ethereum ledger={}.".format(
+                    self.context.agent_name, balance
+                )
+            )
+
+        self._unregister_service()
+
+    def _register_service(self) -> None:
+        """
+        Register to the OEF Service Directory.
+
+        :return: None
+        """
+        strategy = cast(Strategy, self.context.strategy)
+        desc = strategy.get_service_description()
+        self._registered_service_description = desc
+        oef_msg_id = strategy.get_next_oef_msg_id()
+        msg = OEFMessage(
+            type=OEFMessage.Type.REGISTER_SERVICE,
+            id=oef_msg_id,
+            service_description=desc,
+            service_id=SERVICE_ID,
+        )
+        self.context.outbox.put_message(
+            to=DEFAULT_OEF,
+            sender=self.context.agent_address,
+            protocol_id=OEFMessage.protocol_id,
+            message=OEFSerializer().encode(msg),
+        )
+        logger.info(
+            "[{}]: updating generic seller services on OEF.".format(
+                self.context.agent_name
+            )
+        )
+
+    def _unregister_service(self) -> None:
+        """
+        Unregister service from OEF Service Directory.
+
+        :return: None
+        """
+        strategy = cast(Strategy, self.context.strategy)
+        oef_msg_id = strategy.get_next_oef_msg_id()
+        msg = OEFMessage(
+            type=OEFMessage.Type.UNREGISTER_SERVICE,
+            id=oef_msg_id,
+            service_description=self._registered_service_description,
+            service_id=SERVICE_ID,
+        )
+        self.context.outbox.put_message(
+            to=DEFAULT_OEF,
+            sender=self.context.agent_address,
+            protocol_id=OEFMessage.protocol_id,
+            message=OEFSerializer().encode(msg),
+        )
+        logger.info(
+            "[{}]: unregistering generic seller services from OEF.".format(
+                self.context.agent_name
+            )
+        )
+        self._registered_service_description = None
