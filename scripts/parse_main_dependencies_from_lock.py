d85f2b9bad2b10b8dd4ff44e0dfbc4c2a77fab73
MarcoFavorito <marco.favorito@gmail.com>
2020-02-10 23:27:19 +0100

    point to scripts directory from root directory.

--- scripts/parse_main_dependencies_from_lock.py
+++ scripts/parse_main_dependencies_from_lock.py
@@ -11,19 +11,25 @@ import json
 def parse_args():
     """Parse CLI arguments."""
     import argparse
+
     parser = argparse.ArgumentParser("parse_main_dependencies_from_lock")
-    parser.add_argument("pipfile_lock_path", type=argparse.FileType("r"),
-                        help="Path to Pipfile.lock.")
+    parser.add_argument(
+        "pipfile_lock_path", type=argparse.FileType("r"), help="Path to Pipfile.lock."
+    )
     parser.add_argument("-o", "--output", type=argparse.FileType("w"), default=None)
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     arguments = parse_args()
 
     pipfile_lock_content = json.load(arguments.pipfile_lock_path)
-    requirements = sorted(map(lambda x: x[0] + x[1]["version"],
-                              pipfile_lock_content.get("default").items()))
+    requirements = sorted(
+        map(
+            lambda x: x[0] + x[1]["version"],
+            pipfile_lock_content.get("default").items(),
+        )
+    )
 
     requirements_content = "\n".join(requirements)
     if arguments.output is None:
