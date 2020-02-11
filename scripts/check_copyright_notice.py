c3a8e9e6445ee35f1f9a43e3df1b66d6483dc761
MarcoFavorito <marco.favorito@gmail.com>
2020-02-11 01:29:08 +0100

    fix static checks on script check_copyright_notice

--- scripts/check_copyright_notice.py
+++ scripts/check_copyright_notice.py
@@ -72,23 +72,31 @@ def check_copyright(file: Path) -> bool:
 def parse_args():
     """Parse arguments."""
     import argparse
+
     parser = argparse.ArgumentParser("check_copyright_notice")
-    parser.add_argument("--directory", type=str, default=".",
-                        help="The path to the repository root.")
+    parser.add_argument(
+        "--directory", type=str, default=".", help="The path to the repository root."
+    )
 
 
-if __name__ == '__main__':
-    python_files = itertools.chain(Path("aea").glob("**/*.py"),
-                                   Path("packages").glob("**/*.py"),
-                                   Path("tests").glob("**/*.py"),
-                                   Path("scripts").glob("**/*.py"),
-                                   Path("examples").glob("**/*.py"),
-                                   [Path("setup.py")])
+if __name__ == "__main__":
+    python_files = itertools.chain(
+        Path("aea").glob("**/*.py"),
+        Path("packages").glob("**/*.py"),
+        Path("tests").glob("**/*.py"),
+        Path("scripts").glob("**/*.py"),
+        Path("examples").glob("**/*.py"),
+        [Path("setup.py")],
+    )
 
     # filter out protobuf files (*_pb2.py)
-    python_files = filter(lambda x: not str(x).endswith("_pb2.py"), python_files)
+    python_files_filtered = filter(
+        lambda x: not str(x).endswith("_pb2.py"), python_files
+    )
 
-    bad_files = [filepath for filepath in python_files if not check_copyright(filepath)]
+    bad_files = [
+        filepath for filepath in python_files_filtered if not check_copyright(filepath)
+    ]
 
     if len(bad_files) > 0:
         print("The following files are not well formatted:")
