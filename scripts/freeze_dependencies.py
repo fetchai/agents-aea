a2e22699f88ed01627e45bb5770a82fe731bfca4
MarcoFavorito <marco.favorito@gmail.com>
2020-02-23 19:59:09 +0100

    first attempt to fix tox.ini

---
+++ scripts/freeze_dependencies.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""
+This CLI tool freezes the dependencies
+"""
+import json
+import subprocess
+from subprocess import Popen
+
+
+def parse_args():
+    """Parse CLI arguments."""
+    import argparse
+
+    parser = argparse.ArgumentParser("freeze_dependencies")
+    parser.add_argument("-o", "--output", type=argparse.FileType("w"), default=None)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    arguments = parse_args()
+
+    pip_freeze_call = Popen(["pip", "freeze"], stdout=subprocess.PIPE)
+    (stdout, stderr) = pip_freeze_call.communicate()
+    requirements = stdout.decode("utf-8")
+    if arguments.output is None:
+        print(requirements)
+    else:
+        arguments.output.write(requirements)
