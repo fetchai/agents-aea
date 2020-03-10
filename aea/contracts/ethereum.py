246700dbf112ed275b1e64e86afebaadebeae345
Aristotelis <aristotelis.30fyllidis@gmail.com>
2020-03-10 13:04:44 +0000

    Resolve conflicts

--- aea/contracts/ethereum.py
+++ aea/contracts/ethereum.py
@@ -89,7 +89,7 @@ class Contract(BaseContract):
             address=contract_address, abi=self.abi
         )
 
-    def set_instance_w_address(
+    def set_deployed_instance(
         self, ledger_api: EthereumApi, contract_address: str
     ) -> None:
         """
@@ -98,6 +98,7 @@ class Contract(BaseContract):
                 :param ledger_api: the ledger_api we are using.
                 :param contract_address: the contract address
                 """
+        assert self._instance is None, "Instance already set!"
         self._instance = ledger_api.api.eth.contract(
             address=contract_address, abi=self.abi
         )
