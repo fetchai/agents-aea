6ea1f5309600c1e6c91f49a94793e4406fc327fa
Aristotelis <aristotelis.30fyllidis@gmail.com>
2020-03-27 12:13:44 +0000

    Convert ints to str

--- packages/fetchai/skills/thermometer/dialogues.py
+++ packages/fetchai/skills/thermometer/dialogues.py
@@ -46,7 +46,7 @@ class Dialogue(FipaDialogue):
         :return: None
         """
         FipaDialogue.__init__(self, dialogue_label=dialogue_label, is_seller=is_seller)
-        self.temp_data = None  # type: Optional[Dict[str, Any]]
+        self.temp_data = None  # type: Optional[Dict[str, str]]
         self.proposal = None  # type: Optional[Description]
 
 
