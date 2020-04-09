f70e428d2bc0e13d3f4feff0b85b91454e76d08b
Lokman Rahmani <lokman.rahmani@fetch.ai>
2020-04-09 05:55:40 +0100

    Apply style (black) and Address comments

--- aea/connections/stub/connection.py
+++ aea/connections/stub/connection.py
@@ -60,7 +60,7 @@ def _encode(e: Envelope, separator: bytes = SEPARATOR):
     # TOFIX(LR) properly handle bytes encoding with \n escaping
     #  - parse based on ',' separators, not \n
     #  - use size-data format --> need to write an echo script
-    result += str(e.message)[2:-1].encode('latin-1')
+    result += str(e.message)[2:-1].encode("latin-1")
     result += separator
     result += b"\n"
 
@@ -82,9 +82,12 @@ def _decode(e: bytes, separator: bytes = SEPARATOR):
     protocol_id = PublicId.from_str(split[2].decode("utf-8").strip())
     message = split[3].decode("unicode_escape").encode("utf-8")
 
-    return Envelope(to=to, sender=sender, protocol_id=protocol_id, message=message)
+    return Envelope(
+        to=to, sender=sender, protocol_id=protocol_id, message=message
+    )
 
-def _lock_fd(fd: IO[AnyStr]) -> bool:
+
+def _lock_file(fd: IO[AnyStr]) -> bool:
     try:
         fcntl.flock(fd, fcntl.LOCK_EX)
     except OSError as e:
@@ -94,7 +97,8 @@ def _lock_fd(fd: IO[AnyStr]) -> bool:
         logger.debug("Lock successfully acquired for file {}".format(fd.name))
         return True
 
-def _unlock_fd(fd: IO[AnyStr]) -> bool:
+
+def _unlock_file(fd: IO[AnyStr]) -> bool:
     try:
         fcntl.flock(fd, fcntl.LOCK_UN)
     except OSError as e:
@@ -144,7 +148,10 @@ class StubConnection(Connection):
         :param input_file_path: the input file for the incoming messages.
         :param output_file_path: the output file for the outgoing messages.
         """
-        if kwargs.get("configuration") is None and kwargs.get("connection_id") is None:
+        if (
+            kwargs.get("configuration") is None
+            and kwargs.get("connection_id") is None
+        ):
             kwargs["connection_id"] = PublicId("fetchai", "stub", "0.1.0")
         super().__init__(**kwargs)
         input_file_path = Path(input_file_path)
@@ -160,12 +167,14 @@ class StubConnection(Connection):
         self._observer = Observer()
 
         directory = os.path.dirname(input_file_path.absolute())
-        self._event_handler = _ConnectionFileSystemEventHandler(self, input_file_path)
+        self._event_handler = _ConnectionFileSystemEventHandler(
+            self, input_file_path
+        )
         self._observer.schedule(self._event_handler, directory)
 
     def read_envelopes(self) -> None:
         """Receive new envelopes, if any."""
-        ok = _lock_fd(self.input_file)
+        ok = _lock_file(self.input_file)
         if not ok:
             # TOFIX(LR) how to handle locking errors
             return
@@ -174,14 +183,14 @@ class StubConnection(Connection):
         if len(lines) > 0:
             self.input_file.truncate(0)
             self.input_file.seek(0)
-        
-        ok = _unlock_fd(self.input_file)
+
+        ok = _unlock_file(self.input_file)
         # TOFIX(LR) how to handle locking errors
-    
+
         for line in lines:
             logger.debug("read line: {!r}".format(line))
             self._process_line(line)
-    
+
     def _process_line(self, line) -> None:
         """Process a line of the file.
 
@@ -191,11 +200,15 @@ class StubConnection(Connection):
             envelope = _decode(line, separator=SEPARATOR)
             assert self.in_queue is not None, "Input queue not initialized."
             assert self._loop is not None, "Loop not initialized."
-            asyncio.run_coroutine_threadsafe(self.in_queue.put(envelope), self._loop)
+            asyncio.run_coroutine_threadsafe(
+                self.in_queue.put(envelope), self._loop
+            )
         except ValueError as e:
             logger.error("Bad formatted line: {}. {}".format(line, e))
         except Exception as e:
-            logger.error("Error when processing a line. Message: {}".format(str(e)))
+            logger.error(
+                "Error when processing a line. Message: {}".format(str(e))
+            )
 
     async def receive(self, *args, **kwargs) -> Optional["Envelope"]:
         """Receive an envelope."""
@@ -253,10 +266,10 @@ class StubConnection(Connection):
 
         encoded_envelope = _encode(envelope, separator=SEPARATOR)
         logger.debug("write {}".format(encoded_envelope))
-        ok = _lock_fd(self.output_file)
+        ok = _lock_file(self.output_file)
         self.output_file.write(encoded_envelope)
         self.output_file.flush()
-        ok = _unlock_fd(self.output_file)
+        ok = _unlock_file(self.output_file)
         # TOFIX(LR) handle (un)locking errors
 
     @classmethod
@@ -270,14 +283,18 @@ class StubConnection(Connection):
         :param configuration: the connection configuration object.
         :return: the connection object
         """
-        input_file = configuration.config.get("input_file", "./input_file")  # type: str
+        input_file = configuration.config.get(
+            "input_file", "./input_file"
+        )  # type: str
         output_file = configuration.config.get(
             "output_file", "./output_file"
         )  # type: str
         restricted_to_protocols_names = {
             p.name for p in configuration.restricted_to_protocols
         }
-        excluded_protocols_names = {p.name for p in configuration.excluded_protocols}
+        excluded_protocols_names = {
+            p.name for p in configuration.excluded_protocols
+        }
         return StubConnection(
             input_file,
             output_file,
