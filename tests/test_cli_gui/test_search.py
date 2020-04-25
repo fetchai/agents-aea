8534b6b2183c333e37f698505936250d52a2eef0
David Minarsch <david.minarsch@googlemail.com>
2020-04-25 23:09:01 +0100

    fix cli searchtest

--- tests/test_cli_gui/test_search.py
+++ tests/test_cli_gui/test_search.py
@@ -140,7 +140,7 @@ def test_real_search():
     assert response_list.status_code == 200
     data = json.loads(response_list.get_data(as_text=True))
 
-    assert len(data) == 10
+    assert len(data) == 11
     i = 0
 
     assert data[i]["id"] == "fetchai/gym:0.1.0"
