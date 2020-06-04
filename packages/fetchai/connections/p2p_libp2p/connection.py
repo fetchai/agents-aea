f0ce0af056d76d0777429e5e721e24ceb60362d0
Lokman Rahmani <lokman.rahmani@fetch.ai>
2020-06-04 15:36:38 +0100

    Address bandit

--- packages/fetchai/connections/p2p_libp2p/connection.py
+++ packages/fetchai/connections/p2p_libp2p/connection.py
@@ -681,7 +681,7 @@ class P2PLibp2pConnection(Connection):
             if libp2p_host_delegate is not None:
                 delegate_uri = Uri(host=libp2p_host_delegate, port=libp2p_port_delegate)
             else:
-                delegate_uri = Uri(host="0.0.0.0", port=libp2p_port_delegate)  # nosec
+                delegate_uri = Uri(host="127.0.0.1", port=libp2p_port_delegate)
 
         entry_peers_maddrs = [MultiAddr(maddr) for maddr in entry_peers]
 
