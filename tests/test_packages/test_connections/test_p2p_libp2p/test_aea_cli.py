1374cb021d922c494eb190e88e3b6834ddcacb63
Lokman Rahmani <lokman.rahmani@fetch.ai>
2020-06-08 14:56:11 +0100

    libp2p2_p2p aea cli tests: Address reviewers comments

--- tests/test_packages/test_connections/test_p2p_libp2p/test_aea_cli.py
+++ tests/test_packages/test_connections/test_p2p_libp2p/test_aea_cli.py
@@ -32,7 +32,7 @@ import pytest
 from aea.mail.base import Envelope
 from aea.multiplexer import Multiplexer
 from aea.protocols.default.message import DefaultMessage
-from aea.test_tools.test_cases import BaseAEATestCase
+from aea.test_tools.test_cases import AEATestCaseEmpty
 
 from ....conftest import (
     _make_libp2p_connection,
@@ -46,35 +46,38 @@ DEFAULT_NET_SIZE = 4
 
 DEFAULT_LAUNCH_TIMEOUT = 25
 
-def with_new_agent(fn):
-    @functools.wraps(fn)
-    def wrapper(self, *args, **kwargs):
-        self._create_agent()
-        fn(self, *args, **kwargs)
-        self.delete_agents(self.agent_name)
-    return wrapper
-
 @skip_test_windows
-class TestP2PLibp2pConnectionAEARunning(BaseAEATestCase):
+class TestP2PLibp2pConnectionAEARunningDefaultConfigNode(AEATestCaseEmpty):
     """Test AEA with p2p_libp2p connection is correctly run"""
 
-    @classmethod
-    def setup_class(cls):
-        """Set up the test class."""
-        BaseAEATestCase.setup_class()
-
-    @with_new_agent
-    def test_default_config_node(self):
+    def test_agent(self):
         self.add_item("connection", "fetchai/p2p_libp2p:0.2.0")
         self.set_config(
             "agent.default_connection", "fetchai/p2p_libp2p:0.2.0"
         )  # TOFIX(LR) not sure is needed
 
         process = self.run_agent()
-        self._assert_node_running(process)
 
-    @with_new_agent
-    def test_full_node(self):
+        is_running = self.is_running(process, timeout=DEFAULT_LAUNCH_TIMEOUT)
+        assert is_running, "AEA not running within timeout!"
+        
+        check_strings = "My libp2p addresses: ["
+        missing_strings = self.missing_from_output(process, check_strings)
+        assert (
+            missing_strings == []
+        ), "Strings {} didn't appear in agent output.".format(missing_strings)
+
+        self.terminate_agents(process)
+        assert self.is_successfully_terminated(
+            process
+        ), "AEA wasn't successfully terminated."
+
+
+@skip_test_windows
+class TestP2PLibp2pConnectionAEARunningFullNode(AEATestCaseEmpty):
+    """Test AEA with p2p_libp2p connection is correctly run"""
+    
+    def test_agent(self):
         self.add_item("connection", "fetchai/p2p_libp2p:0.2.0")
 
         # setup a full node: with public uri, relay service, and delegate service
@@ -91,17 +94,11 @@ class TestP2PLibp2pConnectionAEARunning(BaseAEATestCase):
         )
 
         process = self.run_agent()
-        self._assert_full_node_running(process)
-
-    def _create_agent(self):
-        self.agent_name = "agent-" + "".join(random.choices(string.ascii_lowercase, k=5))
-        self.set_agent_context(self.agent_name)
-        self.create_agents(self.agent_name)
-
-    def _assert_node_running(self, process, check_strings = "My libp2p addresses: ["):
+        
         is_running = self.is_running(process, timeout=DEFAULT_LAUNCH_TIMEOUT)
         assert is_running, "AEA not running within timeout!"
 
+        check_strings = "My libp2p addresses: ['/dns4/"
         missing_strings = self.missing_from_output(process, check_strings)
         assert (
             missing_strings == []
@@ -112,10 +109,3 @@ class TestP2PLibp2pConnectionAEARunning(BaseAEATestCase):
             process
         ), "AEA wasn't successfully terminated."
 
-    def _assert_full_node_running(self, process):
-        check_strings = "My libp2p addresses: ['/dns4/"
-        self._assert_node_running(process, check_strings)
-
-    def _relay_node_running(self, process):
-        check_strings = "My libp2p addresses: []"
-        self._assert_node_running(process, check_strings)
