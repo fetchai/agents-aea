72f8675fbb0186a2f56aeec488c4fc4ceb4bc344
MarcoFavorito <marco.favorito@gmail.com>
2020-06-15 23:23:16 +0200

    add fetchai/ledger_api:0.1.0 connection skeleton

---
+++ packages/fetchai/connections/ledger_api/connection.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Scaffold connection and channel."""
+
+from typing import Optional
+
+from aea.configurations.base import ConnectionConfig, PublicId
+from aea.connections.base import Connection
+from aea.crypto.wallet import CryptoStore
+from aea.identity.base import Identity
+from aea.mail.base import Envelope
+
+
+class LedgerApiConnection(Connection):
+    """Proxy to the functionality of the SDK or API."""
+
+    connection_id = PublicId.from_str("fetchai/ledger_api:0.1.0")
+
+    def __init__(
+        self,
+        configuration: ConnectionConfig,
+        identity: Identity,
+        crypto_store: CryptoStore,
+    ):
+        """
+        Initialize a connection to interact with a ledger APIs.
+
+        :param configuration: the connection configuration.
+        :param crypto_store: object to access the connection crypto objects.
+        :param identity: the identity object.
+        """
+        super().__init__(
+            configuration=configuration, crypto_store=crypto_store, identity=identity
+        )
+
+    async def connect(self) -> None:
+        """Set up the connection."""
+        raise NotImplementedError  # pragma: no cover
+
+    async def disconnect(self) -> None:
+        """Tear down the connection."""
+        raise NotImplementedError  # pragma: no cover
+
+    async def send(self, envelope: "Envelope") -> None:
+        """
+        Send an envelope.
+
+        :param envelope: the envelope to send.
+        :return: None
+        """
+        raise NotImplementedError  # pragma: no cover
+
+    async def receive(self, *args, **kwargs) -> Optional["Envelope"]:
+        """
+        Receive an envelope. Blocking.
+
+        :return: the envelope received, or None.
+        """
+        raise NotImplementedError  # pragma: no cover
