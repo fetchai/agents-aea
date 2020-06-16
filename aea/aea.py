ae2700fe9d9eee166fd15289ed230698e82e8e3c
David Minarsch <david.minarsch@googlemail.com>
2020-06-16 11:05:02 +0100

    fix service address

--- aea/aea.py
+++ aea/aea.py
@@ -77,7 +77,7 @@ class AEA(Agent):
         default_connection: Optional[PublicId] = None,
         default_routing: Optional[Dict[PublicId, PublicId]] = None,
         connection_ids: Optional[Collection[PublicId]] = None,
-        search_service_address: str = "",
+        search_service_address: str = "oef",
         **kwargs,
     ) -> None:
         """
