0faafb4106e2ac63df3f8957e2cea2859ba9ffae
MarcoFavorito <marco.favorito@gmail.com>
2020-06-22 15:48:48 +0200

    update 'add skill' to add dependency skills recursively

--- aea/cli/add.py
+++ aea/cli/add.py
@@ -156,3 +156,8 @@ def _add_item_deps(ctx: Context, item_type: str, item_config) -> None:
         for contract_public_id in item_config.contracts:
             if contract_public_id not in ctx.agent_config.contracts:
                 add_item(ctx, "contract", contract_public_id)
+
+        # add missing skill
+        for skill_public_id in item_config.skills:
+            if skill_public_id not in ctx.agent_config.skills:
+                add_item(ctx, "skill", skill_public_id)
