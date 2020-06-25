082f4991db08a27c53988b769129c6d0cfe23faf
MarcoFavorito <marco.favorito@gmail.com>
2020-06-25 13:58:02 +0200

    fix pr #1432
    problems due to mismatch between component id and public ids.

--- aea/aea_builder.py
+++ aea/aea_builder.py
@@ -1262,22 +1262,21 @@ class AEABuilder:
                 skip_consistency_check=skip_consistency_check,
             )
 
-    def _find_import_order(self, skill_ids, aea_project_path):
-        """Find import order for skills.
-
-        We need to handle skills separately, since skills can depend on each other.
+    def _find_import_order(
+        self, skill_ids: List[ComponentId], aea_project_path: Path
+    ) -> List[ComponentId]:
+        """Find import order for skills.        We need to handle skills separately, since skills can depend on each other.
         That is, we need to:
         - load the skill configurations to find the import order
         - detect if there are cycles
         - import skills from the leaves of the dependency graph, by finding a topological ordering.
         """
         # the adjacency list for the dependency graph
-        depends_on: Dict[PublicId, Set[PublicId]] = defaultdict(set)
+        depends_on: Dict[ComponentId, Set[ComponentId]] = defaultdict(set)
         # the adjacency list for the inverse dependency graph
-        supports: Dict[PublicId, Set[PublicId]] = defaultdict(set)
+        supports: Dict[ComponentId, Set[ComponentId]] = defaultdict(set)
         # nodes with no incoming edges
         roots = copy(skill_ids)
-
         for skill_id in skill_ids:
             component_path = self._find_component_directory_from_component_id(
                 aea_project_path, skill_id
@@ -1291,9 +1290,14 @@ class AEABuilder:
 
             if len(configuration.skills) != 0:
                 roots.remove(skill_id)
-            depends_on[skill_id].update(configuration.skills)
+            depends_on[skill_id].update(
+                [
+                    ComponentId(ComponentType.SKILL, skill)
+                    for skill in configuration.skills
+                ]
+            )
             for dependency in configuration.skills:
-                supports[dependency].add(skill_id)
+                supports[ComponentId(ComponentType.SKILL, dependency)].add(skill_id)
 
         # find topological order (Kahn's algorithm)
         queue = deque()
