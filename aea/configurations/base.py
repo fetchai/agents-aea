53f65be54e475ea776a633e57def4f3ad0c0ec1b
MarcoFavorito <marco.favorito@gmail.com>
2020-07-07 22:35:43 +0200

    fix doctest

--- aea/configurations/base.py
+++ aea/configurations/base.py
@@ -478,7 +478,7 @@ class PublicId(JSONSerializable):
         >>> public_id_1 < public_id_3
         Traceback (most recent call last):
         ...
-        ValueError: The public IDs author_1/name_1:0.1.0 and author_1/name_2:0.1.0 cannot be compared. Their author and name attributes are different.
+        ValueError: The public IDs author_1/name_1:0.1.0 and author_1/name_2:0.1.0 cannot be compared. Their author or name attributes are different.
 
         """
         if (
