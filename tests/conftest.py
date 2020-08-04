73ec366450211f57b23e4485253407dc648058c3
David Minarsch <david.minarsch@googlemail.com>
2020-08-04 22:06:28 +0100

    add reruns to generate wealth test

--- tests/conftest.py
+++ tests/conftest.py
@@ -205,7 +205,7 @@ DUMMY_SKILL_PUBLIC_ID = PublicId("dummy_author", "dummy", "0.1.0")
 
 MAX_FLAKY_RERUNS = 3
 MAX_FLAKY_RERUNS_ETH = 1
-MAX_FLAKY_RERUNS_INTEGRATION = 0
+MAX_FLAKY_RERUNS_INTEGRATION = 1
 
 FETCHAI_PREF = os.path.join(ROOT_DIR, "packages", "fetchai")
 PROTOCOL_SPECS_PREF_1 = os.path.join(ROOT_DIR, "examples", "protocol_specification_ex")
