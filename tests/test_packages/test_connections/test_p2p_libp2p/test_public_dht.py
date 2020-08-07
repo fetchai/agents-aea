cb95e396d32133e8dc128ba5d3417c0e0a515b21
Lokman Rahmani <lokman.rahmani@fetch.ai>
2020-08-07 08:44:57 +0100

    Apply code style

--- tests/test_packages/test_connections/test_p2p_libp2p/test_public_dht.py
+++ tests/test_packages/test_connections/test_p2p_libp2p/test_public_dht.py
@@ -51,7 +51,7 @@ AEA_LIBP2P_LAUNCH_TIMEOUT = 660  # may downloads up to ~66Mb
 
 @skip_test_windows
 # TODO() Mark tests back as integration ones
-#@pytest.mark.integration
+# @pytest.mark.integration
 @libp2p_log_on_failure_all
 class TestLibp2pConnectionPublicDHTRelay:
     """Test that public DHT's relay service is working properly"""
@@ -190,7 +190,7 @@ class TestLibp2pConnectionPublicDHTRelay:
             pass
 
 
-#@pytest.mark.integration
+# @pytest.mark.integration
 class TestLibp2pConnectionPublicDHTDelegate:
     """Test that public DHT's delegate service is working properly"""
 
@@ -302,7 +302,7 @@ class TestLibp2pConnectionPublicDHTDelegate:
 
 
 @skip_test_windows
-#@pytest.mark.integration
+# @pytest.mark.integration
 class TestLibp2pConnectionPublicDHTRelayAEACli(AEATestCaseEmpty):
     """"Test that public DHT's relay service is working properly, using aea cli"""
 
@@ -348,7 +348,7 @@ class TestLibp2pConnectionPublicDHTRelayAEACli(AEATestCaseEmpty):
         super(TestLibp2pConnectionPublicDHTRelayAEACli, cls).teardown_class()
 
 
-#@pytest.mark.integration
+# @pytest.mark.integration
 class TestLibp2pConnectionPublicDHTDelegateAEACli(AEATestCaseEmpty):
     """Test that public DHT's delegate service is working properly, using aea cli"""
 
