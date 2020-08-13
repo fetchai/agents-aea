40b6aad78e19aab1e26871f27665d4896f260318
Lokman Rahmani <lokman.rahmani@fetch.ai>
2020-08-13 09:59:06 +0100

    Address bandit comment

--- aea/helpers/async_utils.py
+++ aea/helpers/async_utils.py
@@ -20,7 +20,7 @@
 import asyncio
 import datetime
 import logging
-import subprocess
+import subprocess  # nosec
 import time
 from asyncio import CancelledError
 from asyncio.events import AbstractEventLoop, TimerHandle
