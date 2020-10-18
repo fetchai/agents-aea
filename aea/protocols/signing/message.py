b477f5ddafe2aa4d552dad46231760dcdc859189
David Minarsch <david.minarsch@googlemail.com>
2020-10-18 15:50:35 +0100

    fix isort

--- aea/protocols/signing/message.py
+++ aea/protocols/signing/message.py
@@ -34,6 +34,7 @@ from aea.protocols.signing.custom_types import (
 )
 from aea.protocols.signing.custom_types import Terms as CustomTerms
 
+
 _default_logger = logging.getLogger("aea.packages.fetchai.protocols.signing.message")
 
 DEFAULT_BODY_SIZE = 4
