172bf905beb871cf93b20b6d20be585982efeb4e
David Minarsch <david.minarsch@googlemail.com>
2020-11-11 20:32:27 +0000

    fix linting

--- tests/test_packages/test_connections/test_p2p_libp2p/test_errors.py
+++ tests/test_packages/test_connections/test_p2p_libp2p/test_errors.py
@@ -228,8 +228,6 @@ def test_libp2pconnection_mixed_ip_address():
     assert _ip_all_private_or_all_public(["fetch.ai", "127.0.0.1"]) is False
     assert _ip_all_private_or_all_public(["104.26.2.97", "127.0.0.1"]) is False
     assert (
-        _ip_all_private_or_all_public(
-            ["fetch.ai", "agents-p2p-dht.prod.fetch-ai.com"]
-        )
+        _ip_all_private_or_all_public(["fetch.ai", "agents-p2p-dht.prod.fetch-ai.com"])
         is True
     )
