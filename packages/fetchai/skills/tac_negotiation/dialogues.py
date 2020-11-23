a44ec09123b10484590c990a737763730e3aa8ff
David Minarsch <david.minarsch@googlemail.com>
2020-11-23 09:29:39 +0000

    attempt to fix coverage gaps

--- packages/fetchai/skills/tac_negotiation/dialogues.py
+++ packages/fetchai/skills/tac_negotiation/dialogues.py
@@ -170,15 +170,15 @@ class FipaDialogues(Model, BaseFipaDialogues):
             """
             fipa_message = cast(FipaMessage, message)
             if fipa_message.performative != FipaMessage.Performative.CFP:
-                raise ValueError("First message must be a CFP!")
+                raise ValueError("First message must be a CFP!")  # pragma: nocover
             query = fipa_message.query
             if query.model is None:
-                raise ValueError("Query must have a data model!")
+                raise ValueError("Query must have a data model!")  # pragma: nocover
             if query.model.name not in [
                 SUPPLY_DATAMODEL_NAME,
                 DEMAND_DATAMODEL_NAME,
             ]:
-                raise ValueError(
+                raise ValueError(  # pragma: nocover
                     "Query data model name must be in [{},{}]".format(
                         SUPPLY_DATAMODEL_NAME, DEMAND_DATAMODEL_NAME
                     )
