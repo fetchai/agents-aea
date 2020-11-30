f1c9634588b3ca81d7d1b69c33a304b0e5ef0cfc
Stefanos Malliaros <stefanos.malliaros@fetch.ai>
2020-11-30 10:21:00 +0000

    add bash sripting for building and uploading acn_node image

---
+++ scripts/acn/build_upload_img.sh
@@ -0,0 +1,36 @@
+
+#!/usr/bin/env bash
+DOCKERFILE="Dockerfile.dev"
+VERSION=$(git describe --always --dirty=-WIP)
+
+#Check For WIP
+if [[ $VERSION == *-WIP ]];
+then 
+  echo "WIP detected - please commit changes"
+  exit 1
+fi
+
+read -p 'Where to upload the image (prod, or sandbox)?: ' envvar
+shopt -s nocasematch
+case "$envvar" in
+ "prod" ) 
+   echo "Production config selected"
+   REGISTRY="gcr.io/fetch-ai-images"
+   echo "Registry to upload is $REGISTRY"
+   ;;
+ "sandbox" ) 
+   echo "Sandbox config selected"
+   REGISTRY="gcr.io/fetch-ai-sandbox"
+   echo "Registry to upload is $REGISTRY"
+   ;;
+ *) 
+   echo "Wrong env selected. Try again" 
+   echo "Exiting with exit code 1"
+   exit 1
+   ;;
+esac
+
+sleep 2
+
+docker build -t ${REGISTRY}/acn_node:${VERSION} -f ${DOCKERFILE} ../../
+docker push ${REGISTRY}/acn_node:${VERSION}
