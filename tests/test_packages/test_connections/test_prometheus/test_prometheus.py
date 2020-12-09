218464dd69f7c5eb25dd22268722cfc8a957d3f0
James Riehl <jrriehl@gmail.com>
2020-12-09 09:38:15 +0000

    Added prometheus tests

---
+++ tests/test_packages/test_connections/test_prometheus/test_prometheus.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+"""This module contains the tests of the prometheus connection module."""
+import asyncio
+import logging
+import os
+from typing import cast
+from unittest.mock import patch
+
+import prometheus_client
+import pytest
+
+from aea.common import Address
+from aea.configurations.base import ConnectionConfig
+from aea.identity.base import Identity
+from aea.mail.base import Envelope, Message
+from aea.protocols.dialogue.base import Dialogue as BaseDialogue
+
+from packages.fetchai.connections.prometheus.connection import PrometheusConnection
+from packages.fetchai.protocols.prometheus.dialogues import PrometheusDialogue
+from packages.fetchai.protocols.prometheus.dialogues import PrometheusDialogues as BasePrometheusDialogues
+from packages.fetchai.protocols.prometheus.message import PrometheusMessage
+from packages.fetchai.skills.coin_price.behaviours import CoinPriceBehaviour
+
+from tests.conftest import ROOT_DIR, UNKNOWN_PROTOCOL_PUBLIC_ID
+
+
+logger = logging.getLogger(__name__)
+
+
+class PrometheusDialogues(BasePrometheusDialogues):
+    """The dialogues class keeps track of all prometheus dialogues."""
+
+    def __init__(self, self_address: Address, **kwargs) -> None:
+        """
+        Initialize dialogues.
+
+        :return: None
+        """
+
+        def role_from_first_message(  # pylint: disable=unused-argument
+            message: Message, receiver_address: Address
+        ) -> BaseDialogue.Role:
+            """Infer the role of the agent from an incoming/outgoing first message
+
+            :param message: an incoming/outgoing first message
+            :param receiver_address: the address of the receiving agent
+            :return: The role of the agent
+            """
+            return PrometheusDialogue.Role.AGENT
+
+        BasePrometheusDialogues.__init__(
+            self,
+            self_address=self_address,
+            role_from_first_message=role_from_first_message,
+        )
+
+
+class TestPrometheusConnection:
+    """Test the packages/connection/prometheus/connection.py."""
+
+    def setup(self):
+        """Initialise the class."""
+        self.metrics = {}
+        configuration = ConnectionConfig(connection_id=PrometheusConnection.connection_id)
+        self.agent_address = "my_address"
+        identity = Identity("name", address=self.agent_address)
+        self.prometheus_con = PrometheusConnection(
+            identity=identity, configuration=configuration
+        )
+        self.loop = asyncio.get_event_loop()
+        self.prometheus_address = str(PrometheusConnection.connection_id)
+        self.dialogues = PrometheusDialogues(self.agent_address)
+        self.loop.run_until_complete(self.prometheus_con.connect())
+
+    def teardown(self):
+        """Clean up after tests."""
+        self.loop.run_until_complete(self.prometheus_con.disconnect())
+
+    @pytest.mark.asyncio
+    async def test_decode_envelope_error(self):
+        """Test the decoding error for the envelopes."""
+        envelope = Envelope(
+            to=self.prometheus_address,
+            sender=self.agent_address,
+            protocol_id=UNKNOWN_PROTOCOL_PUBLIC_ID,
+            message=b"hello",
+        )
+
+        with pytest.raises(ValueError):
+            await self.prometheus_con.send(envelope)
+
+    # @pytest.mark.asyncio
+    # async def test_send_connection_error(self):
+    #     """Test send connection error."""
+    #     msg, sending_dialogue = self.dialogues.create(
+    #         counterparty=self.prometheus_address,
+    #         performative=PrometheusMessage.Performative.UPDATE_METRIC,
+    #         title="some_metric",
+    #         callable="some_update_func",
+    #         value=0,
+    #     )
+    #     envelope = Envelope(
+    #         to=msg.to, sender=msg.sender, protocol_id=msg.protocol_id, message=msg,
+    #     )
+
+    #     with pytest.raises(ConnectionError):
+    #         await self.prometheus_con.send(envelope)
+
+    @pytest.mark.asyncio
+    async def test_send_response(self):
+        """Test send response message."""
+        sending_dialogue = await self.send_add_metric()
+        assert sending_dialogue.last_message is not None
+        # msg = sending_dialogue.reply(
+        #     performative=PrometheusMessage.Performative.RESPONSE,
+        #     code=0,
+        #     message="some_message",
+        # )
+        # envelope = Envelope(
+        #     to=msg.to, sender=msg.sender, protocol_id=msg.protocol_id, message=msg,
+        # )
+
+        # observation = 1
+        # reward = 1.0
+        # done = True
+        # info = "some info"
+        # with patch.object(
+        #     self.env, "step", return_value=(observation, reward, done, info)
+        # ) as mock:
+        #     await self.prometheus_con.send(envelope)
+        #     mock.assert_called()
+
+        # response = await asyncio.wait_for(self.prometheus_con.receive(), timeout=3)
+        # response_msg = cast(PrometheusMessage, response.message)
+        # response_dialogue = self.dialogues.update(response_msg)
+
+        # assert response_msg.performative == PrometheusMessage.Performative.UPDATE_METRIC
+        # assert response_msg.step_id == msg.step_id
+        # assert response_msg.observation.any == observation
+        # assert response_msg.reward == reward
+        # assert response_msg.done == done
+        # assert response_msg.info.any == info
+        # assert sending_dialogue == response_dialogue
+
+    # @pytest.mark.asyncio
+    # async def test_send_reset(self):
+    #     """Test send reset message."""
+    #     _ = await self.send_reset()
+
+    # @pytest.mark.asyncio
+    # async def test_send_close(self):
+    #     """Test send close message."""
+    #     sending_dialogue = await self.send_reset()
+    #     assert sending_dialogue.last_message is not None
+    #     msg = sending_dialogue.reply(performative=PrometheusMessage.Performative.RESPONSE,)
+    #     envelope = Envelope(
+    #         to=msg.to, sender=msg.sender, protocol_id=msg.protocol_id, message=msg,
+    #     )
+    #     await self.prometheus_con.connect()
+
+    #     with patch.object(self.env, "close") as mock:
+    #         await self.prometheus_con.send(envelope)
+    #         mock.assert_called()
+
+    # @pytest.mark.asyncio
+    # async def test_send_close_negative(self):
+    #     """Test send close message with invalid reference and message id and target."""
+    #     incorrect_msg = PrometheusMessage(
+    #         performative=PrometheusMessage.Performative.RESPONSE,
+    #         dialogue_reference=self.dialogues.new_self_initiated_dialogue_reference(),
+    #     )
+    #     incorrect_msg.to = self.prometheus_address
+    #     incorrect_msg.sender = self.agent_address
+
+    #     # the incorrect message cannot be sent into a dialogue, so this is omitted.
+
+    #     envelope = Envelope(
+    #         to=incorrect_msg.to,
+    #         sender=incorrect_msg.sender,
+    #         protocol_id=incorrect_msg.protocol_id,
+    #         message=incorrect_msg,
+    #     )
+    #     await self.prometheus_con.connect()
+
+    #     with patch.object(self.prometheus_con.channel.logger, "warning") as mock_logger:
+    #         await self.prometheus_con.send(envelope)
+    #         mock_logger.assert_any_call(
+    #             f"Could not create dialogue from message={incorrect_msg}"
+    #         )
+
+    async def send_add_metric(self) -> PrometheusDialogue:
+        """Send an add_metric message."""
+        msg, sending_dialogue = self.dialogues.create(
+            counterparty=self.prometheus_address,
+            performative=PrometheusMessage.Performative.ADD_METRIC,
+            title="some_metric",
+            type="Gauge",
+            description="a gauge",
+            labels=(),
+        )
+        assert sending_dialogue is not None
+        return sending_dialogue
+        # envelope = Envelope(
+        #     to=msg.to, sender=msg.sender, protocol_id=msg.protocol_id, message=msg,
+        # )
+
+        # response = await asyncio.wait_for(self.prometheus_con.receive(), timeout=3)
+        # response_msg = cast(PrometheusMessage, response.message)
+        # response_dialogue = self.dialogues.update(response_msg)
+
+        # assert response_msg.performative == PrometheusMessage.Performative.RESPONSE
+        # assert response_msg.content == "New Gauge successfully added: some_metric."
+        # assert sending_dialogue == response_dialogue
+        # return sending_dialogue
+
+    # @pytest.mark.asyncio
+    # async def test_receive_connection_error(self):
+    #     """Test receive connection error and Cancel Error."""
+    #     with pytest.raises(ConnectionError):
+    #         await self.prometheus_con.receive()
+
