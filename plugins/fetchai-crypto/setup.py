2e51f6afcf1a2f22a621ebd333b3653c8f570fc1
MarcoFavorito <marco.favorito@gmail.com>
2021-01-05 19:40:02 +0100

    try to fix liccheck and pylint errors

--- plugins/fetchai-crypto/setup.py
+++ plugins/fetchai-crypto/setup.py
@@ -27,6 +27,7 @@ setup(
     name="fetchai_crypto",
     version="0.1.0",
     author="Fetch.AI Limited",
+    license="Apache-2.0",
     description="Python package wrapping the public and private key cryptography and ledger api.",
     packages=find_packages(include=["fetchai_crypto*"]),
     install_requires=["aea>=0.8.0,<0.9.0", "ecdsa==0.15", "bech32==1.2.0"],
