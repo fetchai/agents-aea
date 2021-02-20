7f251a11545f3832cb3312b4f402a29ce38acdc0
David Minarsch <david.minarsch@googlemail.com>
2021-02-20 23:40:07 +0000

    fix linters

--- packages/fetchai/connections/p2p_libp2p/connection.py
+++ packages/fetchai/connections/p2p_libp2p/connection.py
@@ -348,7 +348,7 @@ class Libp2pNode:
             raise ValueError("pipe is not set.")  # pragma: nocover
         try:
             return await self.pipe.read()
-        except Exception as e:
+        except Exception as e:  # pragma: nocover pylint: disable=broad-except
             self.logger.exception(f"Failed to read. Exception: {e}")
             return None
 
