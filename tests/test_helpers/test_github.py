7d94a781389667464522bae863915b44f8194d00
Yuri (solarw) Turchenkov <solarw.mail@gmail.com>
2021-02-24 16:55:30 +0300

    github download test

---
+++ tests/test_helpers/test_github.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2020 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+"""This module contains the tests for the 'helpers/github' module."""
+import os
+import tempfile
+
+import pytest
+
+from aea.helpers.base import cd
+from aea.helpers.github import download
+
+
+def test_download():
+    """Test github repo dir downloaded."""
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with cd(temp_dir):
+            total_files = download(
+                "https://github.com/fetchai/agents-aea/tree/main/data"
+            )
+            assert total_files >= 2
+            assert "data" in os.listdir("./"), os.listdir("./")
+            assert os.listdir("./data"), os.listdir("./data")
+            assert "aea.png" in os.listdir("./data"), os.listdir("./data")
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
