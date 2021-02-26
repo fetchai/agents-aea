1313a26d2f6a3d27efb6556d5c4221f0e6fe4cd5
MarcoFavorito <marco.favorito@gmail.com>
2021-02-26 10:49:22 +0100

    add 'decision_maker_handler' to check excludes in AgentConfig:

--- aea/configurations/base.py
+++ aea/configurations/base.py
@@ -1103,6 +1103,7 @@ class AgentConfig(PackageConfiguration):
     CHECK_EXCLUDES = [
         ("private_key_paths",),
         ("connection_private_key_paths",),
+        ("decision_maker_handler",),
         ("default_routing",),
         ("dependencies",),
         ("logging_config",),
