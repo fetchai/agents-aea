0948eb5766285fef57fac429de483bca0ac5263b
MarcoFavorito <marco.favorito@gmail.com>
2021-03-08 16:08:04 +0100

    add cli plugin prototype and example

---
+++ examples/cli_plugin_ex/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2020 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+
+"""Setup script for the plug-in."""
+
+
+from setuptools import setup
+
+
+setup(
+    name="aea-cli-example",
+    version="0.1.0",
+    packages=["cli_plugin_example"],
+    entry_points={"aea.cli": ["print_hello = cli_plugin_example.core:print_hello"]},
+)
