a462cbd80038b0110561cb49da55fe2bd453c4dc
David Minarsch <david.minarsch@googlemail.com>
2021-03-21 17:30:12 +0000

    Remove temporary protocol spec fix

--- aea/configurations/base.py
+++ aea/configurations/base.py
@@ -791,15 +791,9 @@ class ProtocolConfig(ComponentConfiguration):
         )
         self.dependencies = dependencies if dependencies is not None else {}
         self.description = description
-
-        # temporary solution till all protocols updated
-        if protocol_specification_id is not None:
-            self.protocol_specification_id = PublicId.from_str(
-                str(protocol_specification_id)
-            )
-        else:
-            # make protocol specification same as protocol id
-            self.protocol_specification_id = self.public_id
+        self.protocol_specification_id = PublicId.from_str(
+            str(protocol_specification_id)
+        )
 
     @property
     def json(self) -> Dict:
