ad65494f1fb8667587a7d50ecad849bf7051fefc
ali <ali.hosseini@fetch.ai>
2021-04-16 11:57:07 +0100

    test: add coverage for aw5 aea to 100%

--- tests/test_packages/test_skills/test_registration_aw1/test_strategy.py
+++ tests/test_packages/test_skills/test_registration_aw1/test_strategy.py
@@ -19,6 +19,11 @@
 """This module contains the tests of the strategy class of the registration_aw1 skill."""
 
 from pathlib import Path
+from unittest.mock import patch
+
+from aea.crypto.ledger_apis import LedgerApis
+
+from packages.fetchai.skills.registration_aw1.strategy import Strategy
 
 from tests.conftest import ROOT_DIR
 from tests.test_packages.test_skills.test_registration_aw1.intermediate_class import (
@@ -35,6 +40,75 @@ class TestStrategy(RegiatrationAW1TestCase):
     def setup(cls):
         """Setup the test class."""
         super().setup()
+
+    def test__init__i(self):
+        """Test __init__ where developer_handle_only is False."""
+        announce_termination_key = True
+        developer_handle_only = False
+
+        with patch.object(LedgerApis, "is_valid_address", return_value=True):
+            strategy = Strategy(
+                developer_handle=self.developer_handle,
+                ethereum_address=self.ethereum_address,
+                signature_of_fetchai_address=self.signature_of_fetchai_address,
+                shared_storage_key=self.shared_storage_key,
+                whitelist=self.whitelist,
+                tweet=self.tweet,
+                announce_termination_key=announce_termination_key,
+                developer_handle_only=developer_handle_only,
+                name=Strategy,
+                skill_context=self.skill.skill_context,
+            )
+        assert strategy._developer_handle == self.developer_handle
+        assert strategy._whitelist == self.whitelist
+        assert strategy._shared_storage_key == self.shared_storage_key
+        assert strategy.announce_termination_key == announce_termination_key
+        assert strategy.developer_handle_only == developer_handle_only
+
+        assert strategy._ethereum_address == self.ethereum_address
+        assert (
+            strategy._signature_of_fetchai_address == self.signature_of_fetchai_address
+        )
+        assert strategy._tweet == self.tweet
+        assert strategy._is_ready_to_register is False
+
+        assert strategy._is_registered is False
+        assert strategy.is_registration_pending is False
+        assert strategy.signature_of_ethereum_address is None
+        assert strategy._ledger_id == self.skill.skill_context.default_ledger_id
+
+    def test__init__ii(self):
+        """Test __init__ where developer_handle_only is True."""
+        announce_termination_key = False
+        developer_handle_only = True
+
+        strategy = Strategy(
+            developer_handle=self.developer_handle,
+            ethereum_address=self.ethereum_address,
+            signature_of_fetchai_address=self.signature_of_fetchai_address,
+            shared_storage_key=self.shared_storage_key,
+            whitelist=self.whitelist,
+            tweet=self.tweet,
+            announce_termination_key=announce_termination_key,
+            developer_handle_only=developer_handle_only,
+            name=Strategy,
+            skill_context=self.skill.skill_context,
+        )
+        assert strategy._developer_handle == self.developer_handle
+        assert strategy._whitelist == self.whitelist
+        assert strategy._shared_storage_key == self.shared_storage_key
+        assert strategy.announce_termination_key == announce_termination_key
+        assert strategy.developer_handle_only == developer_handle_only
+
+        assert strategy._is_ready_to_register is True
+        assert strategy._ethereum_address == "some_dummy_address"
+        assert strategy._signature_of_fetchai_address is None
+        assert strategy._tweet is None
+
+        assert strategy._is_registered is False
+        assert strategy.is_registration_pending is False
+        assert strategy.signature_of_ethereum_address is None
+        assert strategy._ledger_id == self.skill.skill_context.default_ledger_id
 
     def test_properties(self):
         """Test the properties of Strategy class."""
