ea0c2b68a1c5713b03725a9de9a14088f74c36f9
Yuri (solarw) Turchenkov <solarw.mail@gmail.com>
2021-05-07 17:56:53 +0300

    USE_CLIENT fix

--- examples/tac_deploy/entrypoint.sh
+++ examples/tac_deploy/entrypoint.sh
@@ -118,7 +118,7 @@ function set_agent(){
 	aea add-key fetchai $key_file_name	
 	key_file_name=$(generate_key $LEDGER $name $agent_data_dir 1)
 	aea add-key fetchai $key_file_name --connection
-	if [ "$USE_CLIENT" == false ];
+	if [ "$USE_CLIENT" == "false" ];
 	then
 		json=$(printf '{"log_file": "%s", "delegate_uri": null, "entry_peers": ["%s"], "local_uri": "127.0.0.1:%s", "public_uri": null, "node_connection_timeout": '%i'}' "$agent_data_dir/libp2p_node.log" "$PEER" "$port" "$(($NODE_CONNECTION_TIMEOUT))")
 		aea config set --type dict vendor.fetchai.connections.p2p_libp2p.config "$json"
