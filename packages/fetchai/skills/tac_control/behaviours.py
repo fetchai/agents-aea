28816f7d258ec9dfd7e1c72a395532a8e692b81e
James Riehl <jrriehl@gmail.com>
2021-12-15 09:42:23 +0000

    feat: more generalized soef registration behaviour

--- packages/fetchai/skills/tac_control/behaviours.py
+++ packages/fetchai/skills/tac_control/behaviours.py
@@ -20,6 +20,7 @@
 """This package contains a the behaviours."""
 
 import datetime
+from enum import Enum
 from typing import Any, Optional, cast
 
 from aea.helpers.search.models import Description
@@ -37,6 +38,7 @@ from packages.fetchai.skills.tac_control.parameters import Parameters
 
 
 DEFAULT_MAX_SOEF_REGISTRATION_RETRIES = 5
+SOEF_LOBBY_TIMEOUT = 60
 
 
 class TacBehaviour(Behaviour):
@@ -44,7 +46,6 @@ class TacBehaviour(Behaviour):
 
     def setup(self) -> None:
         """Implement the setup."""
-        pass
 
     def act(self) -> None:
         """Implement the act."""
@@ -57,6 +58,10 @@ class TacBehaviour(Behaviour):
             and game.is_registered_agent
         ):
             game.phase = Phase.GAME_REGISTRATION
+            registration_behaviour = cast(
+                SoefRegisterBehaviour, self.context.behaviours.soef_register
+            )
+            registration_behaviour.Status = SoefRegisterBehaviour.Status.REGISTERING_TAC
             self.context.logger.info(
                 "TAC open for registration until: {}".format(parameters.start_time)
             )
@@ -151,6 +156,10 @@ class TacBehaviour(Behaviour):
                 performative=TacMessage.Performative.CANCELLED,
             )
             self.context.outbox.put_message(message=tac_msg)
+        registration_behaviour = cast(
+            SoefRegisterBehaviour, self.context.behaviours.soef_register
+        )
+        registration_behaviour.status = SoefRegisterBehaviour.Status.UNREGISTERING
         if game.phase == Phase.GAME:
             self.context.logger.info(
                 "finished competition:\n{}".format(game.holdings_summary)
@@ -162,11 +171,19 @@ class TacBehaviour(Behaviour):
 
     def teardown(self) -> None:
         """Implement the setup."""
-        pass
 
 
 class SoefRegisterBehaviour(TickerBehaviour):
     """This class implements the SOEF register behaviour."""
+
+    class Status(Enum):
+        """Registration status."""
+
+        REGISTERING_AGENT = "registering_agent"
+        REGISTERING_TAC = "registering_tac"
+        REGISTERED = "registered"
+        UNREGISTERING = "unregistering"
+        UNREGISTERED = "unregistered"
 
     def __init__(self, **kwargs: Any):
         """Instantiate the behaviour."""
@@ -179,6 +196,10 @@ class SoefRegisterBehaviour(TickerBehaviour):
         self.failed_registration_reason = (
             None
         )  # type: Optional[OefSearchMessage.OefErrorOperation]
+        self.status = (
+            self.Status.REGISTERING_AGENT
+        )  # type: SoefRegisterBehaviour.Status
+        self._last_registration_attempt = None  # type: datetime.datetime
         self._nb_retries = 0
 
     def setup(self) -> None:
@@ -187,14 +208,14 @@ class SoefRegisterBehaviour(TickerBehaviour):
 
     def act(self) -> None:
         """Implement the act."""
-        self._retry_failed_registration()
 
-        game = cast(Game, self.context.game)
-        if game.phase.value == Phase.GAME_REGISTRATION.value:
+        if self.failed_registration_msg is not None:
+            self._retry_failed_registration()
+            return
+
+        if self.status == self.Status.REGISTERING_TAC:
             self._register_tac()
-        elif game.phase.value == Phase.POST_GAME.value:
-            self._unregister_tac()
-        elif game.phase.value == Phase.GAME_SETUP.value:
+        elif self.status == self.Status.UNREGISTERING:
             self._unregister_tac()
 
     def teardown(self) -> None:
@@ -204,41 +225,45 @@ class SoefRegisterBehaviour(TickerBehaviour):
 
     def _retry_failed_registration(self) -> None:
         """Retry a failed registration."""
-        if self.failed_registration_msg is not None:
-            self._nb_retries += 1
-            if self._nb_retries > self._max_soef_registration_retries:
-                self.context.is_active = False
-                return
+        self._nb_retries += 1
+        if self._nb_retries > self._max_soef_registration_retries:
+            self.context.is_active = False
+            return
 
-            if (
-                self.failed_registration_reason
-                == OefSearchMessage.OefErrorOperation.ALREADY_IN_LOBBY
-            ):
-                self.context.logger.info(
-                    "Already in OEF lobby from previous registration attempt."
-                )
-
-            elif (
-                self.failed_registration_reason
-                == OefSearchMessage.OefErrorOperation.ALREADY_REGISTERED
-            ):
-                self.context.logger.info("Already registered with sOEF.")
-                return
-
-            oef_search_dialogues = cast(
-                OefSearchDialogues, self.context.oef_search_dialogues
-            )
-            oef_search_msg, _ = oef_search_dialogues.create(
-                counterparty=self.failed_registration_msg.to,
-                performative=self.failed_registration_msg.performative,
-                service_description=self.failed_registration_msg.service_description,
-            )
-            self.context.outbox.put_message(message=oef_search_msg)
+        if (
+            self.failed_registration_reason
+            == OefSearchMessage.OefErrorOperation.ALREADY_IN_LOBBY
+        ):
             self.context.logger.info(
-                f"Retrying registration on SOEF. Retry {self._nb_retries} out of {self._max_soef_registration_retries}."
+                "Already in OEF lobby from previous registration attempt."
             )
+            now = datetime.datetime.now()
+            if now - self._last_registration_attempt < datetime.timedelta(
+                seconds=SOEF_LOBBY_TIMEOUT
+            ):
+                return
 
-            self.failed_registration_msg = None
+        elif (
+            self.failed_registration_reason
+            == OefSearchMessage.OefErrorOperation.ALREADY_REGISTERED
+        ):
+            self.context.logger.info("Already registered with sOEF.")
+            return
+
+        oef_search_dialogues = cast(
+            OefSearchDialogues, self.context.oef_search_dialogues
+        )
+        oef_search_msg, _ = oef_search_dialogues.create(
+            counterparty=self.failed_registration_msg.to,
+            performative=self.failed_registration_msg.performative,
+            service_description=self.failed_registration_msg.service_description,
+        )
+        self.context.outbox.put_message(message=oef_search_msg)
+        self.context.logger.info(
+            f"Retrying registration on SOEF. Retry {self._nb_retries} out of {self._max_soef_registration_retries}."
+        )
+
+        self.failed_registration_msg = None
 
     def _register(self, description: Description, logger_msg: str) -> None:
         """
@@ -300,6 +325,7 @@ class SoefRegisterBehaviour(TickerBehaviour):
         )
         self.context.outbox.put_message(message=oef_search_msg)
         self._registered_description = None
+        self.status = self.Status.UNREGISTERED
         self.context.logger.info("unregistering TAC data model from SOEF.")
 
     def _unregister_agent(self) -> None:
