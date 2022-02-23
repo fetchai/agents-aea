6f65bda94059b45cce38eeff958a488a3e29b2d7
8baller <8ball030@gmail.com>
2022-02-23 12:28:55 +0000

    chore: copywrite

--- tests/test_docs/test_quickstart.py
+++ tests/test_docs/test_quickstart.py
@@ -2,6 +2,7 @@
 # ------------------------------------------------------------------------------
 #
 #   Copyright 2021-2022 Valory AG
+#   Copyright 2018-2021 Fetch.AI Limited
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
