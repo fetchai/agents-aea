285265079af0b8c1e519ad2d7dd71645d863cf90
Adamantios <adamantios.zaras@valory.xyz>
2022-03-15 12:09:07 +0200

    fix: add missing `raise_on_try=True`

--- packages/fetchai/connections/ledger/ledger_dispatcher.py
+++ packages/fetchai/connections/ledger/ledger_dispatcher.py
@@ -279,7 +279,7 @@ class LedgerApiRequestDispatcher(RequestDispatcher):
         """
         try:
             transaction_digest = api.send_signed_transaction(
-                message.signed_transaction.body
+                message.signed_transaction.body, raise_on_try=True
             )
         except Exception as e:  # pylint: disable=broad-except
             return self.get_error_message(e, api, message, dialogue)
