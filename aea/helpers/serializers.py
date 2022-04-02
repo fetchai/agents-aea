61abae1dfd4a6a52e2b89ef319f2e45fa3221dbe
m.a.p.karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-04-02 12:03:11 +0200

    fix:updating hashes

--- aea/helpers/serializers.py
+++ aea/helpers/serializers.py
@@ -22,7 +22,10 @@
 import copy
 from typing import Any, Dict, Tuple
 
-from google.protobuf.struct_pb2 import ListValue, Struct  # pylint: disable=no-name-in-module
+from google.protobuf.struct_pb2 import (  # pylint: disable=no-name-in-module
+    ListValue,
+    Struct,
+)
 
 
 class DictProtobufStructSerializer:
