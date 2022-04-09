76054b3001caeac0d3381d1d4a1cfa26236bd2da
m.a.p.karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-04-09 12:28:01 +0200

    test:tendermint protocol

--- tests/test_packages/test_protocols/test_tendermint.py
+++ tests/test_packages/test_protocols/test_tendermint.py
@@ -19,29 +19,47 @@
 
 """This module contains tests for the register protocol."""
 
-import itertools
+from itertools import product
 from unittest import mock
 
 import pytest
 
 from aea.common import Address
-from aea.mail.base import Envelope
 from aea.protocols.base import Message
 from aea.protocols.dialogue.base import Dialogue as BaseDialogue
 
-from packages.valory.protocols.tendermint.dialogues import TendermintDialogue
 from packages.valory.protocols.tendermint.dialogues import (
-    TendermintDialogues as BaseTendermintDialogues,
+    TendermintDialogue,
+    TendermintDialogues,
 )
 from packages.valory.protocols.tendermint.message import TendermintMessage
 
 
 PERFORMATIVE = TendermintMessage.Performative
-
+INITIAL_DIALOGUE_REFERENCE = ("0", "")
 ALICE_TENDERMINT_INFO = dict(ip="192.168.0.0", hostname="alice")
 BOB_TENDERMINT_INFO = dict(ip="192.168.255.255", hostname="bob")
 
+PERFORMATIVE_TEST_KWARGS = [
+    dict(
+        error_code=TendermintMessage.ErrorCode.INVALID_REQUEST,
+        error_msg="Invalid request",
+        info={"error": "Agent address not registered for this service."},
+    ),
+    dict(query=""),
+    dict(info=ALICE_TENDERMINT_INFO),
+]
 
+
+# utility function
+def mock_role(  # pylint: disable=unused-argument
+    message: Message, receiver_address: Address
+) -> BaseDialogue.Role:
+    """Infer the role of the agent from an incoming/outgoing first message"""
+    return TendermintDialogue.Role.AGENT
+
+
+# tests
 def test_performative_name_value_matching():
     """Test performative name, value, and match."""
     for performative in TendermintMessage.Performative:
@@ -51,43 +69,22 @@ def test_performative_name_value_matching():
 
 
 @pytest.mark.parametrize(
-    "performative, kwargs",
-    [
-        (PERFORMATIVE.TENDERMINT_CONFIG_REQUEST, dict(query="")),
-        (PERFORMATIVE.TENDERMINT_CONFIG_RESPONSE, dict(info=ALICE_TENDERMINT_INFO)),
-        (
-            TendermintMessage.Performative.ERROR,
-            dict(
-                error_code=TendermintMessage.ErrorCode.INVALID_REQUEST,
-                error_msg="Invalid request",
-                info={"error": "Agent address not registered for this service."},
-            ),
-        ),
-    ],
+    "performative, kwargs", zip(PERFORMATIVE, PERFORMATIVE_TEST_KWARGS),
 )
 def test_serialization(performative, kwargs):
     """Test that the serialization for the 'tendermint' protocol works."""
 
     msg = TendermintMessage(
         message_id=1,
-        dialogue_reference=(str(0), ""),
+        dialogue_reference=INITIAL_DIALOGUE_REFERENCE,
         target=0,
         performative=performative,
         **kwargs
     )
-
-    msg.to = "receiver"
-    original_envelope = Envelope(to=msg.to, sender="sender", message=msg)
-    retrieved_envelope = Envelope.decode(original_envelope.encode())
-    # assert original_envelope = retrieved_envelope
-    assert retrieved_envelope.to == original_envelope.to
-    assert retrieved_envelope.sender == original_envelope.sender
-    assert (
-        retrieved_envelope.protocol_specification_id
-        == original_envelope.protocol_specification_id
-    )
-    assert retrieved_envelope.sender == original_envelope.sender
-    assert retrieved_envelope.context == original_envelope.context
+    encoded_msg = TendermintMessage.serializer.encode(msg)
+    assert isinstance(encoded_msg, bytes)
+    decoded_msg = TendermintMessage.serializer.decode(encoded_msg)
+    assert decoded_msg == msg
 
 
 def test_encoding_unknown_performative():
@@ -109,39 +106,21 @@ def test_decoding_unknown_performative():
             TendermintMessage.serializer.decode(encoded_msg)
 
 
-class TendermintDialogues(BaseTendermintDialogues):
-    """The dialogues class keeps track of all dialogues."""
-
-    def __init__(self, self_address: Address) -> None:
-        """Initialize dialogues"""
-
-        def role_from_first_message(  # pylint: disable=unused-argument
-            message: Message, receiver_address: Address
-        ) -> BaseDialogue.Role:
-            """Infer the role of the agent from an incoming/outgoing first message"""
-            return TendermintDialogue.Role.AGENT
-
-        super().__init__(
-            self_address=self_address,
-            role_from_first_message=role_from_first_message,
-            dialogue_class=TendermintDialogue,
-        )
-
-
 class TestDialogues:
     """Tests tendermint dialogues."""
 
     @classmethod
     def setup_class(cls):
         """Set up the test."""
-        cls.dialogues_alice = TendermintDialogues("address_alice")
-        cls.dialogues_bob = TendermintDialogues("address_bob")
+        args = mock_role, TendermintDialogue
+        cls.dialogues_alice = TendermintDialogues("address_alice", *args)
+        cls.dialogues_bob = TendermintDialogues("address_bob", *args)
 
     def test_create_self_initiated(self):
         """Test the self initialisation of a dialogue."""
         result = self.dialogues_alice._create_self_initiated(
             dialogue_opponent_addr=self.dialogues_bob.self_address,
-            dialogue_reference=("0", ""),
+            dialogue_reference=INITIAL_DIALOGUE_REFERENCE,
             role=TendermintDialogue.Role.AGENT,
         )
         assert isinstance(result, TendermintDialogue)
@@ -150,11 +129,10 @@ class TestDialogues:
     def test_end_state_dialogue_stats(self):
         """Test end state dialogue stats"""
         dialogue_stats = self.dialogues_alice.dialogue_stats
-        for end_state in TendermintDialogue.EndState:
-            assert not dialogue_stats.self_initiated[end_state]
-            for i, self_initiated in itertools.product(range(1, 3), [True, False]):
-                dialogue_stats.add_dialogue_endstate(end_state, self_initiated)
-                assert dialogue_stats.self_initiated[end_state] == i
+        iterator = product(TendermintDialogue.EndState, range(1, 3), [True, False])
+        for end_state, i, is_self_initiated in iterator:
+            dialogue_stats.add_dialogue_endstate(end_state, is_self_initiated)
+            assert dialogue_stats.self_initiated[end_state] == i
 
     def test_message_exchange(self):
         """Test message exchange between agents"""
