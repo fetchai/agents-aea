6dbfa02ef025a25d4945b584d0d7d634e31faeca
Viraj Patel <vptl185@gmail.com>
2022-07-12 07:08:54 +0000

    chore: fix dependencies on setup.py

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ base_deps = [
     "requests>=2.22.0,<3.0.0",
     "python-dotenv>=0.14.0,<0.18.0",
     "ecdsa>=0.15,<0.17.0",
+    "morphys>=1.0",
     "py-multibase>=1.0.0",
     "py-multicodec>=0.2.0",
 ]
