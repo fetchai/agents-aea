bf7c63dcaf557f9ae302855fd10b19e2419a4d3f
Viraj Patel <vptl185@gmail.com>
2022-08-22 07:21:01 +0000

    fix: Dockerfiles

--- deploy-image/build.sh
+++ deploy-image/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # setup the agent
-aea fetch open_aea/my_first_aea:latest --local
+aea fetch --remote open_aea/my_first_aea:bafybeibnjfr3sdg57ggyxbcfkh42yqkj6a3gftp55l26aaw2z2jvvc3tny
 cd my_first_aea/
 aea install
 aea build
