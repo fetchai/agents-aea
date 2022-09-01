092fd87ae7bad0c61733d1ecaed44c63389ea4d9
Viraj Patel <vptl185@gmail.com>
2022-09-01 15:04:24 +0000

    feat: add support for testing a collection of packages with aea test command

--- aea/cli/test.py
+++ aea/cli/test.py
@@ -21,11 +21,14 @@
 """Implementation of the 'aea test' command."""
 import os
 import sys
+import tempfile
+import time
 from pathlib import Path
 from typing import Callable, Optional, Sequence, Set, cast
 
 import click
 import pytest
+from coverage.cmdline import main as coverage
 
 from aea.cli.utils.click_utils import (
     PublicIdParameter,
@@ -40,7 +43,6 @@ from aea.configurations.constants import (
     AEA_TEST_DIRNAME,
     CONNECTION,
     CONTRACT,
-    PACKAGES,
     PROTOCOL,
     SKILL,
 )
@@ -54,14 +56,35 @@ from aea.configurations.loader import load_component_configuration
 from aea.configurations.manager import find_component_directory_from_component_id
 from aea.exceptions import enforce
 from aea.helpers.base import cd
+from aea.helpers.dependency_tree import DependencyTree
+
+
+COVERAGERC_FILE = ".coveragerc"
+COVERAGERC_CONFIG = """[run]
+omit =
+    */tests/*
+
+[html]
+directory = {root_dir}/htmlcov
+
+[xml]
+output = {root_dir}/coverage.xml
+"""
 
 
 @click.group(invoke_without_command=True)
 @click.pass_context
+@click.option(
+    "--cov",
+    is_flag=True,
+    default=False,
+    help="Use this flag to enable code coverage checks.",
+)
 @pytest_args
-def test(click_context: click.Context, args: Sequence[str]) -> None:
+def test(click_context: click.Context, cov: bool, args: Sequence[str]) -> None:
     """Run tests of an AEA project."""
     ctx = cast(Context, click_context.obj)
+    ctx.config["cov"] = cov
     if click_context.invoked_subcommand is None:
         test_aea_project(click_context, Path(ctx.cwd), args)
 
@@ -118,7 +141,82 @@ def by_path(
     """Executes a test suite of a package specified by a path."""
     click.echo(f"Executing tests of package at {path}'...")
     full_path = Path(ctx.cwd) / Path(path)
-    test_package_by_path(full_path, args, packages_dir=Path(ctx.registry_path))
+    test_package_by_path(
+        full_path,
+        args,
+        packages_dir=Path(ctx.registry_path),
+        cov=ctx.config.get("cov", False),
+    )
+
+
+@test.command()
+@pytest_args
+@pass_ctx
+def packages(
+    ctx: Context,
+    args: Sequence[str],
+) -> None:
+    """Executes a test suite of a package specified by a path."""
+    packages_dir = Path(ctx.registry_path)
+    available_packages = DependencyTree.find_packages_in_a_local_repository(
+        packages_dir
+    )
+
+    coverage_data = []
+    failures = []
+    start_time = time.perf_counter()
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        covrc_file = Path(temp_dir, COVERAGERC_FILE)
+        covrc_file.write_text(
+            COVERAGERC_CONFIG.format(root_dir=str(packages_dir.parent))
+        )
+        for package_type, package_dir in available_packages[:20]:
+            if package_type == PackageType.AGENT.value:
+                continue
+
+            test_dir = package_dir / AEA_TEST_DIRNAME
+            if not test_dir.exists():
+                continue
+
+            load_package(package_dir, packages_dir=packages_dir)
+            with cd(package_dir):
+                click.echo(
+                    f"Running tests for {package_dir.name} of type {package_type}"
+                )
+                exit_code = pytest.main(
+                    [
+                        package_dir / AEA_TEST_DIRNAME,
+                        f"--cov={package_dir.absolute()}",
+                        f"--doctest-modules",
+                        str(package_dir.absolute()),
+                        f"--cov-config={covrc_file}",
+                        "--cov-report=term",
+                    ]
+                )
+                if exit_code:
+                    click.echo(
+                        f"Running tests for for {package_dir.name} of type {package_type} failed"
+                    )
+                    failures.append(
+                        (exit_code, os.path.sep.join(package_dir.parts[-3:]))
+                    )
+            coverage_data.append(str(package_dir / ".coverage"))
+        total_time = (time.perf_counter() - start_time) // 60
+
+        click.echo(f"Time : {total_time}")
+        click.echo("Generating coverage reports.")
+
+        coverage(argv=["combine", f"--rcfile={covrc_file}", *coverage_data])
+        coverage(argv=["html", f"--rcfile={covrc_file}"])
+        coverage(argv=["xml", f"--rcfile={covrc_file}"])
+
+    if len(failures):
+        click.echo("Failed tests")
+        click.echo("Exit Code\tPackage")
+        for exit_code, package in failures:
+            click.echo(f"{exit_code}       \t{package}")
+        sys.exit(1)
 
 
 def test_item(
@@ -156,24 +254,19 @@ def test_item(
     # for a package in an AEA project, the "packages" dir is the AEA project dir
     aea_project_path = Path(ctx.cwd)
     test_package_by_path(
-        package_dirpath, pytest_arguments, aea_project_path=aea_project_path
+        package_dirpath,
+        pytest_arguments,
+        aea_project_path=aea_project_path,
+        cov=ctx.config.get("cov", False),
     )
 
 
-def test_package_by_path(
+def load_package(
     package_dir: Path,
-    pytest_arguments: Sequence[str],
     aea_project_path: Optional[Path] = None,
     packages_dir: Optional[Path] = None,
 ) -> None:
-    """
-    Fingerprint package placed in package_dir.
-
-    :param package_dir: directory of the package
-    :param pytest_arguments: arguments to forward to Pytest
-    :param aea_project_path: directory to the AEA project
-    :param packages_dir: directory of the packages to import from
-    """
+    """Load packages into cache."""
     enforce(
         (aea_project_path is None) != (packages_dir is None),
         "one of either aea_project_path or packages_dir must be specified",
@@ -189,6 +282,12 @@ def test_package_by_path(
     # check the path points to a valid AEA package
     package_type = determine_package_type_for_directory(package_dir)
 
+    if package_type != PackageType.AGENT:
+        component_type = ComponentType(package_type.value)
+        configuration = load_component_configuration(component_type, package_dir)
+        configuration.directory = package_dir
+        load_aea_packages_recursively(configuration, package_path_finder, root_packages)
+
     test_package_dir = package_dir / AEA_TEST_DIRNAME
     enforce(
         test_package_dir.exists(),
@@ -196,15 +295,53 @@ def test_package_by_path(
         click.ClickException,
     )
 
-    if package_type != PackageType.AGENT:
-        component_type = ComponentType(package_type.value)
-        configuration = load_component_configuration(component_type, package_dir)
-        configuration.directory = package_dir
-        load_aea_packages_recursively(configuration, package_path_finder, root_packages)
+
+def test_package_by_path(
+    package_dir: Path,
+    pytest_arguments: Sequence[str],
+    aea_project_path: Optional[Path] = None,
+    packages_dir: Optional[Path] = None,
+    cov: bool = False,
+) -> None:
+    """
+    Fingerprint package placed in package_dir.
+
+    :param package_dir: directory of the package
+    :param pytest_arguments: arguments to forward to Pytest
+    :param aea_project_path: directory to the AEA project
+    :param packages_dir: directory of the packages to import from
+    """
+
+    load_package(package_dir, aea_project_path, packages_dir)
+    exit_code = run_pytest(package_dir, pytest_arguments=pytest_arguments, cov=cov)
+    sys.exit(exit_code)
+
+
+def run_pytest(
+    package_dir: Path,
+    pytest_arguments: Sequence[str],
+    cov: bool = False,
+) -> int:
+    """Run pytest."""
+
+    runtime_args = [
+        AEA_TEST_DIRNAME,
+        *pytest_arguments,
+    ]
+
+    if cov:
+        runtime_args.extend(
+            [
+                f"--cov={package_dir.absolute()}",
+                f"--doctest-modules",
+                str(package_dir.absolute()),
+                "--cov-report=term",
+                "--cov-report=term-missing",
+            ]
+        )
 
     with cd(package_dir):
-        exit_code = pytest.main([AEA_TEST_DIRNAME] + list(pytest_arguments))
-        sys.exit(exit_code)
+        return pytest.main(runtime_args)
 
 
 @check_aea_project
