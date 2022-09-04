c68a6c75b2995490e405d558e9220e4f90e634e0
David Minarsch <david.minarsch@googlemail.com>
2022-09-04 15:55:15 +0200

    fix: copy tests in protocol generator functionality

--- aea/cli/generate_all_protocols.py
+++ aea/cli/generate_all_protocols.py
@@ -64,6 +64,7 @@ from aea.manager.helpers import AEAProject
 SPECIFICATION_REGEX = re.compile(r"(---\nname.*\.\.\.)", re.DOTALL)
 LIBPROTOC_VERSION = "libprotoc 3.19.4"
 CUSTOM_TYPE_MODULE_NAME = "custom_types.py"
+TESTS_DIRECTORY_NAME = "tests"
 PROTOCOL_GENERATOR_DOCSTRING_REGEX = "It was created with protocol buffer compiler version `libprotoc .*` and aea protocol generator version `.*`."
 
 logging.basicConfig(format="[%(asctime)s][%(levelname)s] %(message)s")
@@ -157,9 +158,9 @@ def _fix_generated_protocol(package_path: Path) -> None:
 
     :param package_path: path to the protocol package. Used also to recover the protocol name.
     """
-    log(f"Restore original custom types in {package_path}")
     custom_types_module = package_path / CUSTOM_TYPE_MODULE_NAME
     if custom_types_module.exists():
+        log(f"Restore original custom types in {package_path}")
         file_to_replace = Path(PROTOCOLS, package_path.name, CUSTOM_TYPE_MODULE_NAME)
         file_to_replace.write_text(custom_types_module.read_text())
 
@@ -169,6 +170,13 @@ def _fix_generated_protocol(package_path: Path) -> None:
         shutil.copyfile(
             package_readme_file,
             Path(PROTOCOLS, package_path.name, DEFAULT_README_FILE),
+        )
+
+    tests_module = package_path / TESTS_DIRECTORY_NAME
+    if tests_module.is_dir():
+        log(f"Restore original `tests` directory in {package_path}")
+        shutil.copytree(
+            tests_module, Path(PROTOCOLS, package_path.name, TESTS_DIRECTORY_NAME)
         )
 
 
