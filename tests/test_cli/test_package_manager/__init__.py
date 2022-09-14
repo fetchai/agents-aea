3e198dcab23a29de0331622e3b8dac1fe0390936
Viraj Patel <vptl185@gmail.com>
2022-09-14 05:07:41 +0000

    fix: file header

--- tests/test_cli/test_package_manager/__init__.py
+++ tests/test_cli/test_package_manager/__init__.py
@@ -2,7 +2,6 @@
 # ------------------------------------------------------------------------------
 #
 #   Copyright 2022 Valory AG
-#   Copyright 2018-2020 Fetch.AI Limited
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
