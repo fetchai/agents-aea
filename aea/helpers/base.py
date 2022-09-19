3e538e2c2fda08718b0745ab20999a810c5b44bf
David Minarsch <david.minarsch@googlemail.com>
2022-09-19 22:10:37 +0100

    fix: test

--- aea/helpers/base.py
+++ aea/helpers/base.py
@@ -475,8 +475,8 @@ def recursive_update(
             and value is not None
             and value_to_update is not None
         ):
-            error_str = "Trying to replace value '{value_to_update}' of type '{value_to_update_type}' with value '{value}' which is of type '{value_type}'."
-            if "${" in value:
+            error_str = f"Trying to replace value '{value_to_update}' of type '{value_to_update_type.__name__}' with value '{value}' which is of type '{value_type.__name__}'."
+            if isinstance(value, str) and "${" in value:
                 error_str += " It appears you are using environmental variables as overwrites. Make sure to use the `--aev` flag."
             raise ValueError(error_str)
 
