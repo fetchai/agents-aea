ad9402510e4d1eb184ed07216f792018e507f1f4
ali <ali.hosseini@fetch.ai>
2022-11-30 20:20:50 +0000

    regenerate aggregation protocol

--- packages/fetchai/protocols/aggregation/aggregation_pb2.py
+++ packages/fetchai/protocols/aggregation/aggregation_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x11\x61ggregation.proto\x12\x1e\x61\x65\x61.fetchai.aggregation.v0_2_4"\xaa\x03\n\x12\x41ggregationMessage\x12\x62\n\x0b\x61ggregation\x18\x05 \x01(\x0b\x32K.aea.fetchai.aggregation.v0_2_4.AggregationMessage.Aggregation_PerformativeH\x00\x12\x62\n\x0bobservation\x18\x06 \x01(\x0b\x32K.aea.fetchai.aggregation.v0_2_4.AggregationMessage.Observation_PerformativeH\x00\x1aZ\n\x18Observation_Performative\x12\r\n\x05value\x18\x01 \x01(\x03\x12\x0c\n\x04time\x18\x02 \x01(\t\x12\x0e\n\x06source\x18\x03 \x01(\t\x12\x11\n\tsignature\x18\x04 \x01(\t\x1a`\n\x18\x41ggregation_Performative\x12\r\n\x05value\x18\x01 \x01(\x03\x12\x0c\n\x04time\x18\x02 \x01(\t\x12\x14\n\x0c\x63ontributors\x18\x03 \x03(\t\x12\x11\n\tsignature\x18\x04 \x01(\tB\x0e\n\x0cperformativeb\x06proto3'
+    b'\n\x11\x61ggregation.proto\x12\x1e\x61\x65\x61.fetchai.aggregation.v0_2_5"\xaa\x03\n\x12\x41ggregationMessage\x12\x62\n\x0b\x61ggregation\x18\x05 \x01(\x0b\x32K.aea.fetchai.aggregation.v0_2_5.AggregationMessage.Aggregation_PerformativeH\x00\x12\x62\n\x0bobservation\x18\x06 \x01(\x0b\x32K.aea.fetchai.aggregation.v0_2_5.AggregationMessage.Observation_PerformativeH\x00\x1aZ\n\x18Observation_Performative\x12\r\n\x05value\x18\x01 \x01(\x03\x12\x0c\n\x04time\x18\x02 \x01(\t\x12\x0e\n\x06source\x18\x03 \x01(\t\x12\x11\n\tsignature\x18\x04 \x01(\t\x1a`\n\x18\x41ggregation_Performative\x12\r\n\x05value\x18\x01 \x01(\x03\x12\x0c\n\x04time\x18\x02 \x01(\t\x12\x14\n\x0c\x63ontributors\x18\x03 \x03(\t\x12\x11\n\tsignature\x18\x04 \x01(\tB\x0e\n\x0cperformativeb\x06proto3'
 )
 
 
@@ -35,7 +35,7 @@ AggregationMessage = _reflection.GeneratedProtocolMessageType(
             {
                 "DESCRIPTOR": _AGGREGATIONMESSAGE_OBSERVATION_PERFORMATIVE,
                 "__module__": "aggregation_pb2"
-                # @@protoc_insertion_point(class_scope:aea.fetchai.aggregation.v0_2_4.AggregationMessage.Observation_Performative)
+                # @@protoc_insertion_point(class_scope:aea.fetchai.aggregation.v0_2_5.AggregationMessage.Observation_Performative)
             },
         ),
         "Aggregation_Performative": _reflection.GeneratedProtocolMessageType(
@@ -44,12 +44,12 @@ AggregationMessage = _reflection.GeneratedProtocolMessageType(
             {
                 "DESCRIPTOR": _AGGREGATIONMESSAGE_AGGREGATION_PERFORMATIVE,
                 "__module__": "aggregation_pb2"
-                # @@protoc_insertion_point(class_scope:aea.fetchai.aggregation.v0_2_4.AggregationMessage.Aggregation_Performative)
+                # @@protoc_insertion_point(class_scope:aea.fetchai.aggregation.v0_2_5.AggregationMessage.Aggregation_Performative)
             },
         ),
         "DESCRIPTOR": _AGGREGATIONMESSAGE,
         "__module__": "aggregation_pb2"
-        # @@protoc_insertion_point(class_scope:aea.fetchai.aggregation.v0_2_4.AggregationMessage)
+        # @@protoc_insertion_point(class_scope:aea.fetchai.aggregation.v0_2_5.AggregationMessage)
     },
 )
 _sym_db.RegisterMessage(AggregationMessage)
