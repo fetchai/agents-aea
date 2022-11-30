0817250a549d7397ad12c488d3aa07a64cbd6ff6
ali <ali.hosseini@fetch.ai>
2022-11-30 14:32:43 +0000

    bump plugin versions and update comspy deps

--- plugins/aea-ledger-cosmos/setup.py
+++ plugins/aea-ledger-cosmos/setup.py
@@ -25,7 +25,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="aea-ledger-cosmos",
-    version="1.2.3",
+    version="1.2.4",
     author="Fetch.AI Limited",
     license="Apache-2.0",
     description="Python package wrapping the public and private key cryptography and ledger api of Cosmos.",
