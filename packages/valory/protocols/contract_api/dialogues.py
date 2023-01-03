286ca9a675b583ca99e5fb97a4426721a2f4455b
Yuri (solarw) Turchenkov <solarw.mail@gmail.com>
2023-01-03 15:56:00 +0300

    fixes

--- packages/valory/protocols/contract_api/dialogues.py
+++ packages/valory/protocols/contract_api/dialogues.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2022 valory
+#   Copyright 2023 valory
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
