040cda54ec558c1f9902d7c6455596ef35b5e0e0
Viraj Patel <vptl185@gmail.com>
2023-01-04 12:17:42 +0530

    chore: copyright

--- aea/__version__.py
+++ aea/__version__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2022 Valory AG
+#   Copyright 2022-2023 Valory AG
 #   Copyright 2018-2019 Fetch.AI Limited
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
