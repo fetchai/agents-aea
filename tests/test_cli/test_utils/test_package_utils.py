b77ab2db68cc3d82865314b5ea72792229661864
Viraj Patel <vptl185@gmail.com>
2023-01-04 14:16:17 +0530

    chore: copyright

--- tests/test_cli/test_utils/test_package_utils.py
+++ tests/test_cli/test_utils/test_package_utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2022 Valory AG
+#   Copyright 2022-2023 Valory AG
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
