e1cb82714a1fab40f63d72fd70fa886f91889bf7
ali <ali.hosseini@fetch.ai>
2023-01-18 16:58:18 +0000

    build: regenerate all protocols

--- packages/fetchai/protocols/cosm_trade/__init__.py
+++ packages/fetchai/protocols/cosm_trade/__init__.py
@@ -20,7 +20,7 @@
 """
 This module contains the support resources for the cosm_trade protocol.
 
-It was created with protocol buffer compiler version `libprotoc 3.19.4` and aea version `1.2.4`.
+It was created with protocol buffer compiler version `libprotoc 3.19.4` and aea version `1.2.5`.
 """
 
 from packages.fetchai.protocols.cosm_trade.message import CosmTradeMessage
