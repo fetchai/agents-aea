d25ad70ebe40ccb9c365bda8f7b18a35c9698ef2
Adamantios <adamantios.zaras@valory.xyz>
2023-03-16 16:04:18 +0200

    refactor: connection to support sending txs with flashbots

--- packages/valory/connections/ledger/ledger_dispatcher.py
+++ packages/valory/connections/ledger/ledger_dispatcher.py
@@ -17,10 +17,14 @@
 #   limitations under the License.
 #
 # ------------------------------------------------------------------------------
+
 """This module contains the implementation of the ledger API request dispatcher."""
+
 import asyncio
 import logging
-from typing import Any, cast
+from typing import Any, List, Optional, cast
+
+from hexbytes import HexBytes
 
 from aea.connections.base import ConnectionStates
 from aea.crypto.base import LedgerApi
@@ -37,6 +41,9 @@ from packages.valory.protocols.ledger_api.dialogues import (
 )
 from packages.valory.protocols.ledger_api.message import LedgerApiMessage
 
+
+DEFAULT_USE_FLASHBOTS = False
+DEFAULT_NEXT_TARGET_BLOCKS = 25
 
 _default_logger = logging.getLogger(
     "aea.packages.valory.connections.ledger.ledger_dispatcher"
@@ -338,6 +345,38 @@ class LedgerApiRequestDispatcher(RequestDispatcher):
             )
         return response
 
+    @staticmethod
+    def _get_transaction_digest(
+        api: LedgerApi, message: LedgerApiMessage
+    ) -> Optional[str]:
+        """Send a signed transaction and get the transaction digest."""
+        if message.rpc_config and message.rpc_config.body.get(
+            "use_flashbots", DEFAULT_USE_FLASHBOTS
+        ):
+            target_blocks = message.rpc_config.body.get("target_block_numbers", None)
+            if target_blocks is None:
+                current_block_number = int(api.api.eth.get_block_number())
+                target_blocks = [
+                    *range(
+                        current_block_number,
+                        current_block_number + DEFAULT_NEXT_TARGET_BLOCKS,
+                    )
+                ]
+            target_blocks = cast(List[int], target_blocks)
+
+            # at the moment we do not support multiple transactions, though flashbots can support this
+            raw_transaction = message.signed_transaction.body["raw_transaction"]
+            raw_transactions = [HexBytes(raw_transaction)]
+            tx_hashes = api.bundle_and_send(raw_transactions, target_blocks)
+            if tx_hashes is not None:
+                return tx_hashes[0]
+            return None
+
+        return api.send_signed_transaction(
+            message.signed_transaction.body,
+            raise_on_try=True,
+        )
+
     def send_signed_transaction(
         self,
         api: LedgerApi,
@@ -353,10 +392,7 @@ class LedgerApiRequestDispatcher(RequestDispatcher):
         :return: response Ledger API message
         """
         try:
-            transaction_digest = api.send_signed_transaction(
-                message.signed_transaction.body,
-                raise_on_try=True,
-            )
+            transaction_digest = self._get_transaction_digest(api, message)
         except Exception as e:  # pylint: disable=broad-except  # pragma: nocover
             return self.get_error_message(e, api, message, dialogue)
 
