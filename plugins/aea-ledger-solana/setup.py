5e1f13b7be97136519684b0d58d9e71deb978ddc
angrybayblade <vptl185@gmail.com>
2023-03-21 11:03:06 +0530

    feat: bump plugins to v1.31.0

--- plugins/aea-ledger-solana/setup.py
+++ plugins/aea-ledger-solana/setup.py
@@ -25,7 +25,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="open-aea-ledger-solana",
-    version="1.24.17",
+    version="1.31.0",
     author="dassy23",
     license="Apache-2.0",
     description="Python package wrapping the public and private key cryptography and ledger api of solana.",
